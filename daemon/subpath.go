afc3e569ce76339378b5a2db3f1e399a40ea1180
Jacob Howard <jacob@havoc.io>
2016-11-03 23:59:15 +0200

    Added daemon subpath calculations.
    The subpath function can be used to compute paths for daemon files (e.g.
    the daemon lock and IPC socket).

---
+++ daemon/subpath.go
@@ -0,0 +1,24 @@
+package daemon
+
+import (
+	"path/filepath"
+
+	"github.com/pkg/errors"
+
+	"github.com/havoc-io/mutagen/filesystem"
+)
+
+const (
+	daemonDirectoryName = "daemon"
+)
+
+func subpath(name string) (string, error) {
+	// Compute the daemon root directory path and ensure it exists.
+	daemonRoot, err := filesystem.Mutagen(daemonDirectoryName)
+	if err != nil {
+		return "", errors.Wrap(err, "unable to compute daemon directory")
+	}
+
+	// Compute the combined path.
+	return filepath.Join(daemonRoot, name), nil
+}
