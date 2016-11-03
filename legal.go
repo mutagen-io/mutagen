297b89addd02abaa8099081763837d27cc10f7a0
Jacob Howard <jacob@havoc.io>
2016-11-03 15:09:13 +0200

    Small formatting fix for legal information.
    Let's try to keep licenses consistently formatted, i.e. single spaced,
    same bullets, and snapped to 80 columns.

--- legal.go
+++ legal.go
@@ -62,9 +62,8 @@ pflag (https://github.com/ogier/pflag)
 Copyright (c) 2012 Alex Ogier. All rights reserved.
 Copyright (c) 2012 The Go Authors. All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
 
 * Redistributions of source code must retain the above copyright notice, this
   list of conditions and the following disclaimer.
