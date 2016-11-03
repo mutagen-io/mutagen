67355be5aec048a8d17322d7693f5f9ff718a71f
Jacob Howard <jacob@havoc.io>
2016-11-03 18:56:10 +0200

    Added snapshot benchmarking script.
    This script provides snapshot times with cold and warm caches,
    serialization and deserialization times, serialized size, and validates
    that the deserialized snapshot is equivalent to the original.
    
    Initial results show that the stable_marshal extension slows down
    serialization by a factor of 2-3 over the standard reflection-based
    Protocol Buffers serialization, but deserialization time is reduced by
    an equivalent factor. Since stable marshalling is necessary, there's not
    much we can do, but marhsalling in general seems sufficiently fast at
    the moment that I'm not concerned.

---
+++ scripts/snapshot_bench.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"time"
+	"os"
+
+	"github.com/pkg/errors"
+
+	"github.com/havoc-io/mutagen/cmd"
+	"github.com/havoc-io/mutagen/sync"
+)
+
+var usage = `snapshot_bench [-h|--help] <path>
+`
+
+func main() {
+	// Parse arguments.
+	flagSet := cmd.NewFlagSet("snapshot_bench", usage, []int{1})
+	path := flagSet.ParseOrDie(os.Args[1:])[0]
+
+	// Print information.
+	fmt.Println("Analyzing", path)
+
+	// Create a snapshot without any cache.
+	start := time.Now()
+	snapshot, cache, err := sync.Snapshot(path, sha1.New(), nil)
+	if err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to create snapshot"))
+	}
+	stop := time.Now()
+	fmt.Println("Cold snapshot took", stop.Sub(start))
+
+	// Create a snapshot with a cache.
+	start = time.Now()
+	snapshot, _, err = sync.Snapshot(path, sha1.New(), cache)
+	if err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to create snapshot"))
+	}
+	stop = time.Now()
+	fmt.Println("Warm snapshot took", stop.Sub(start))
+
+	// Serialize it.
+	start = time.Now()
+	serialized, err := snapshot.Marshal()
+	if err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to serialize snapshot"))
+	}
+	stop = time.Now()
+	fmt.Println("Serialization took", stop.Sub(start))
+
+	// Deserialize it.
+	start = time.Now()
+	deserialized := &sync.Entry{}
+	if err = deserialized.Unmarshal(serialized); err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to deserialize snapshot"))
+	}
+	stop = time.Now()
+	fmt.Println("Deserialization took", stop.Sub(start))
+
+	// Print other information.
+	fmt.Println("Serialized size was", len(serialized), "bytes")
+	fmt.Println(
+		"Original/deserialized snapshots equivalent?",
+		deserialized.Equal(snapshot),
+	)
+}
