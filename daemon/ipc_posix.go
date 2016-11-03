442e2d7e33cc4bc262e023d4fe1e2641180d81d6
Jacob Howard <jacob@havoc.io>
2016-11-04 00:40:02 +0200

    Added daemon IPC infrastructure.
    On POSIX, we use UNIX domain sockets, while on Windows we used named
    pipes. Both of these provide authentication and authorization at the
    connection level, so we don't need to perform additional authentication
    on top.
    
    On Windows, we have to generate a random named pipe name and write it to
    a secure location to stop squatting attacks and superfluous pipe
    connectivity. Named pipes are really dangerous due to their default
    impersonation abilities, but the approach we take should address these
    problems.
    
    Named pipes are kind of an ugly beast, and the Go implementations of
    them are even uglier, but they're sufficient for now. In the future, we
    may want to consider switching to Microsoft's go-winio package
    (https://github.com/Microsoft/go-winio), but unfortunately it's a bit
    nascent at the moment. It provides saner defaults though for
    impersonation settings, allowing authentication without impersonation.

---
+++ daemon/ipc_posix.go
@@ -0,0 +1,44 @@
+// +build !windows
+
+package daemon
+
+import (
+	"net"
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	socketName = "daemon.sock"
+)
+
+func DialTimeout(timeout time.Duration) (net.Conn, error) {
+	// Compute the socket path.
+	socketPath, err := subpath(socketName)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to compute socket path")
+	}
+
+	// Dial.
+	return net.DialTimeout("unix", socketPath, timeout)
+}
+
+func NewListener() (net.Listener, error) {
+	// Compute the socket path.
+	socketPath, err := subpath(socketName)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to compute socket path")
+	}
+
+	// Remove the socket path if it exists. This is safe since the caller should
+	// own the daemon lock. In general, the socket path will be cleaned up when
+	// the listener is closed, but if there's a crash, we need to wipe it.
+	if err := os.Remove(socketPath); err != nil && !os.IsNotExist(err) {
+		return nil, errors.Wrap(err, "unable to remove stale socket")
+	}
+
+	// Create the listener.
+	return net.Listen("unix", socketPath)
+}
