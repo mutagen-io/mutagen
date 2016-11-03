f6749308ed17ef5f0e911160ba55b052fe2e0a2e
Jacob Howard <jacob@havoc.io>
2016-11-03 22:25:23 +0200

    Added a function to extract the exit code from an os/exec error.
    The ExitCodeForError will attempt to extract an exit code for a child
    process when it returns an os/exec.ExitError.

---
+++ process/exit_code.go
@@ -0,0 +1,26 @@
+package process
+
+import (
+	"os/exec"
+	"syscall"
+
+	"github.com/pkg/errors"
+)
+
+func ExitCodeForError(err error) (int, error) {
+	// Attempt to extract the error.
+	exitErr, ok := err.(*exec.ExitError)
+	if !ok {
+		return 0, errors.New("error is not an exit error")
+	}
+
+	// Attempt to extract the wait status. The syscall.WaitStatus type is
+	// platform-dependent, but this code uses a portable subset of its features.
+	waitStatus, ok := exitErr.Sys().(syscall.WaitStatus)
+	if !ok {
+		return 0, errors.New("unable to access wait status")
+	}
+
+	// Done.
+	return waitStatus.ExitStatus(), nil
+}
