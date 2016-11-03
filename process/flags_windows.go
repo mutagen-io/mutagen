2c7e2b4e65cabb6ff6449d14c1bded3d298756e2
Jacob Howard <jacob@havoc.io>
2016-11-03 22:27:04 +0200

    Added missing DETACHED_PROCESS flag for process creation on Windows.
    The DETACHED_PROCESS flag can be included in process creation flags on
    Windows to create a process that is detached from its parent terminal.
    This is necessary for the daemon and SSH subprocesses.

---
+++ process/flags_windows.go
@@ -0,0 +1,7 @@
+package process
+
+const (
+	// More information on process creation flag available here:
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684863
+	DETACHED_PROCESS = 0x00000008
+)
