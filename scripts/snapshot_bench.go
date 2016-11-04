4805b5c51695ea34d6231a4d2f8f7b41fd408a4c
Jacob Howard <jacob@havoc.io>
2016-11-04 18:34:55 +0200

    Updated snapshot_bench to measure disk read/write time for cache.
    Also fixed up variable names and messages a bit.

--- scripts/snapshot_bench.go
+++ scripts/snapshot_bench.go
@@ -3,13 +3,18 @@ package main
 import (
 	"crypto/sha1"
 	"fmt"
-	"time"
+	"io/ioutil"
 	"os"
+	"time"
 
 	"github.com/pkg/errors"
 
 	"github.com/havoc-io/mutagen/cmd"
 	"github.com/havoc-io/mutagen/sync"
+)
+
+const (
+	cacheFile = "cache_test"
 )
 
 var usage = `snapshot_bench [-h|--help] <path>
@@ -43,27 +48,27 @@ func main() {
 
 	// Serialize it.
 	start = time.Now()
-	serialized, err := snapshot.Marshal()
+	serializedSnapshot, err := snapshot.Marshal()
 	if err != nil {
 		cmd.Fatal(errors.Wrap(err, "unable to serialize snapshot"))
 	}
 	stop = time.Now()
-	fmt.Println("Serialization took", stop.Sub(start))
+	fmt.Println("Snapshot serialization took", stop.Sub(start))
 
 	// Deserialize it.
 	start = time.Now()
-	deserialized := &sync.Entry{}
-	if err = deserialized.Unmarshal(serialized); err != nil {
+	deserializedSnapshot := &sync.Entry{}
+	if err = deserializedSnapshot.Unmarshal(serializedSnapshot); err != nil {
 		cmd.Fatal(errors.Wrap(err, "unable to deserialize snapshot"))
 	}
 	stop = time.Now()
-	fmt.Println("Deserialization took", stop.Sub(start))
+	fmt.Println("Snapshot deserialization took", stop.Sub(start))
 
 	// Print other information.
-	fmt.Println("Serialized size was", len(serialized), "bytes")
+	fmt.Println("Serialized snapshot size is", len(serializedSnapshot), "bytes")
 	fmt.Println(
 		"Original/deserialized snapshots equivalent?",
-		deserialized.Equal(snapshot),
+		deserializedSnapshot.Equal(snapshot),
 	)
 
 	// Serialize the cache.
@@ -84,6 +89,27 @@ func main() {
 	stop = time.Now()
 	fmt.Println("Cache deserialization took", stop.Sub(start))
 
+	// Write the serialized cache to disk.
+	start = time.Now()
+	if err = ioutil.WriteFile(cacheFile, serializedCache, 0600); err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to write cache"))
+	}
+	stop = time.Now()
+	fmt.Println("Cache write took", stop.Sub(start))
+
+	// Read the serialized cache from disk.
+	start = time.Now()
+	if _, err = ioutil.ReadFile(cacheFile); err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to write cache"))
+	}
+	stop = time.Now()
+	fmt.Println("Cache read took", stop.Sub(start))
+
+	// Wipe the temporary file.
+	if err = os.Remove(cacheFile); err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to remove cache"))
+	}
+
 	// Print other information.
-	fmt.Println("Serialized cache size was", len(serializedCache), "bytes")
+	fmt.Println("Serialized cache size is", len(serializedCache), "bytes")
 }
