b4bc3aed00be19449734c140db9601d4e963db0b
Jacob Howard <jacob@havoc.io>
2016-11-08 21:51:19 +0200

    Added basic SSH facilities for agent connectivity.
    These aren't yet tested, but should cover the use cases we need.

---
+++ agent/ssh.go
@@ -0,0 +1,155 @@
+package agent
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+
+	"github.com/havoc-io/mutagen/environment"
+	"github.com/havoc-io/mutagen/process"
+	"github.com/havoc-io/mutagen/url"
+)
+
+const (
+	PrompterEnvironmentVariable              = "MUTAGEN_PROMPTER"
+	PrompterContextBase64EnvironmentVariable = "MUTAGEN_PROMPTER_CONTEXT_BASE64"
+)
+
+func prompterEnvironment(prompter, context string) []string {
+	// If there is no prompter, return nil to just use the current environment.
+	if prompter == "" {
+		return nil
+	}
+
+	// Convert context to base64 encoding so that we can pass it through the
+	// environment safely.
+	contextBase64 := base64.StdEncoding.EncodeToString([]byte(context))
+
+	// Create a copy of the current environment.
+	result := make(map[string]string, len(environment.Current))
+	for k, v := range environment.Current {
+		result[k] = v
+	}
+
+	// Insert necessary environment variables.
+	result["SSH_ASKPASS"] = process.Current.ExecutablePath
+	result["DISPLAY"] = "mutagen"
+	result[PrompterEnvironmentVariable] = prompter
+	result[PrompterContextBase64EnvironmentVariable] = contextBase64
+
+	// Convert into the desired format.
+	return environment.Format(result)
+}
+
+// TODO: Document that the local path must be absolute.
+func scp(prompter, context, local string, remote *url.SSHURL) error {
+	// Locate the SCP command.
+	scp, err := scpCommand()
+	if err != nil {
+		return errors.Wrap(err, "unable to identify SCP executable")
+	}
+
+	// HACK: On Windows, we attempt to use SCP executables that might not
+	// understand Windows paths because they're designed to run inside a POSIX-
+	// style environment (e.g. MSYS or Cygwin). To work around this, we run them
+	// in the same directory as the source and just pass them the source base
+	// name. This works fine on other systems as well. Unfortunately this means
+	// that we need to use absolute paths, but we do that anyway.
+	if !filepath.IsAbs(local) {
+		return errors.New("scp source path must be absolute")
+	}
+	workingDirectory, sourceBase := filepath.Split(local)
+
+	// Compute the destination URL.
+	destinationURL := fmt.Sprintf("%s:%s", remote.Hostname, remote.Path)
+	if remote.Username != "" {
+		destinationURL = fmt.Sprintf("%s@%s", remote.Username, destinationURL)
+	}
+
+	// Set up arguments.
+	var scpArguments []string
+	if remote.Port != 0 {
+		scpArguments = append(scpArguments, "-P", fmt.Sprintf("%d", remote.Port))
+	}
+	scpArguments = append(scpArguments, sourceBase, destinationURL)
+
+	// Create the process.
+	scpProcess := exec.Command(scp, scpArguments...)
+
+	// Set the working directory.
+	scpProcess.Dir = workingDirectory
+
+	// Force it to run detached.
+	scpProcess.SysProcAttr = processAttributes()
+
+	// Set the environment necessary for prompting.
+	scpProcess.Env = prompterEnvironment(prompter, context)
+
+	// Run the operation.
+	if err = scpProcess.Run(); err != nil {
+		return errors.Wrap(err, "unable to run SCP process")
+	}
+
+	// Success.
+	return nil
+}
+
+// TODO: Document that the URL path is NOT used as a working directory, it is
+// simply ignored.
+func ssh(prompter, context string, remote *url.SSHURL, command string) (*exec.Cmd, error) {
+	// Locate the SSH command.
+	ssh, err := sshCommand()
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to identify SSH executable")
+	}
+
+	// Compute the target.
+	target := remote.Hostname
+	if remote.Username != "" {
+		target = fmt.Sprintf("%s@%s", remote.Username, remote.Hostname)
+	}
+
+	// Set up arguments.
+	var sshArguments []string
+	if remote.Port != 0 {
+		sshArguments = append(sshArguments, "-p", fmt.Sprintf("%d", remote.Port))
+	}
+	sshArguments = append(sshArguments, target, command)
+
+	// Create the process.
+	sshProcess := exec.Command(ssh, sshArguments...)
+
+	// Force it to run detached.
+	sshProcess.SysProcAttr = processAttributes()
+
+	// Set the environment necessary for prompting.
+	sshProcess.Env = prompterEnvironment(prompter, context)
+
+	// Done.
+	return sshProcess, nil
+}
+
+func sshRun(prompter, context string, remote *url.SSHURL, command string) error {
+	// Create the process.
+	process, err := ssh(prompter, context, remote, command)
+	if err != nil {
+		return errors.Wrap(err, "unable to create command")
+	}
+
+	// Run the process.
+	return process.Run()
+}
+
+func sshOutput(prompter, context string, remote *url.SSHURL, command string) ([]byte, error) {
+	// Create the process.
+	process, err := ssh(prompter, context, remote, command)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to create command")
+	}
+
+	// Run the process.
+	return process.Output()
+}
---
+++ agent/ssh_windows.go
@@ -0,0 +1,60 @@
+package agent
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+
+	"github.com/pkg/errors"
+
+	"github.com/havoc-io/mutagen/process"
+)
+
+// commandSearchPaths specifies additional locations on Windows where we might
+// find ssh.exe and scp.exe binaries.
+var commandSearchPaths = []string{
+	`C:\Program Files\Git\usr\bin`,
+	`C:\Program Files (x86)\Git\usr\bin`,
+	`C:\msys32\usr\bin`,
+	`C:\msys64\usr\bin`,
+	// TODO: Add Cygwin binary paths.
+}
+
+func commandNamed(name string) (string, error) {
+	// First, try to use the standard LookPath mechanism in case the user has
+	// the binaries in their path already.
+	if result, err := exec.LookPath(name); err == nil {
+		return result, nil
+	}
+
+	// If there was nothing in the path, then scan other well-known directories
+	// where we might find a viable binary.
+	for _, path := range commandSearchPaths {
+		target := filepath.Join(path, fmt.Sprintf("%s.exe", name))
+		// TODO: Should we inspect the information used by stat to ensure this
+		// file exists? No real need to check executability, anything with an
+		// exe extension on Windows shows up as executable.
+		if _, err := os.Stat(target); err != nil {
+			return target, nil
+		}
+	}
+
+	// Failure.
+	return "", errors.New("unable to locate command")
+}
+
+func scpCommand() (string, error) {
+	return commandNamed("scp")
+}
+
+func sshCommand() (string, error) {
+	return commandNamed("ssh")
+}
+
+func processAttributes() *syscall.SysProcAttr {
+	return &syscall.SysProcAttr{
+		CreationFlags: process.DETACHED_PROCESS,
+	}
+}
