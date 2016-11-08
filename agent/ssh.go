91bcd068e823acdb0f141ab8bdbc8676dfad4c6c
Jacob Howard <jacob@havoc.io>
2016-11-08 22:39:19 +0200

    Added command line prompting facilities.
    For now these treat all prompts as non-echoing, but the infrastructure
    is in place to perform prompt classification based on known SSH prompts.

--- agent/ssh.go
+++ agent/ssh.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/pkg/errors"
 
+	"github.com/howeyc/gopass"
+
 	"github.com/havoc-io/mutagen/environment"
 	"github.com/havoc-io/mutagen/process"
 	"github.com/havoc-io/mutagen/url"
@@ -17,6 +19,48 @@ const (
 	PrompterEnvironmentVariable              = "MUTAGEN_PROMPTER"
 	PrompterContextBase64EnvironmentVariable = "MUTAGEN_PROMPTER_CONTEXT_BASE64"
 )
+
+type PromptClass uint8
+
+const (
+	PromptClassSecret PromptClass = iota
+	PromptClassDisplay
+	PromptClassBinary
+)
+
+func ClassifyPrompt(prompt string) PromptClass {
+	// TODO: Implement using white-listing regexes based on known OpenSSH
+	// prompts.
+	return PromptClassSecret
+}
+
+func PromptCommandLine(context, prompt string) (string, error) {
+	// Classify the prompt.
+	class := ClassifyPrompt(prompt)
+
+	// Figure out which getter to use.
+	var getter func() ([]byte, error)
+	if class == PromptClassDisplay || class == PromptClassBinary {
+		getter = gopass.GetPasswdEchoed
+	} else {
+		getter = gopass.GetPasswd
+	}
+
+	// Print the context (if any) and the prompt.
+	if context != "" {
+		fmt.Println(context)
+	}
+	fmt.Print(prompt)
+
+	// Get the result.
+	result, err := getter()
+	if err != nil {
+		return "", errors.Wrap(err, "unable to read response")
+	}
+
+	// Success.
+	return string(result), nil
+}
 
 func prompterEnvironment(prompter, context string) []string {
 	// If there is no prompter, return nil to just use the current environment.
