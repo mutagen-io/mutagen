b06809ef9c10b53bca1a145079630d6cc5e64c49
Jacob Howard <jacob@havoc.io>
2016-11-08 20:23:45 +0200

    Added simple version exchange protocol.
    This protocol can be used as a simple handshake method. It's not
    designed for security, merely as a simple and sensible handshake
    mechanism, mostly needed for the agent to ensure that an SSH process has
    actually started.

--- version.go
+++ version.go
@@ -1,7 +1,9 @@
 package mutagen
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 )
 
 const (
@@ -15,4 +17,47 @@ const (
 
 func Version() string {
 	return fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
+}
+
+type versionBytes [12]byte
+
+func SendVersion(writer io.Writer) error {
+	// Compute the version bytes.
+	var data versionBytes
+	binary.BigEndian.PutUint32(data[:4], VersionMajor)
+	binary.BigEndian.PutUint32(data[4:8], VersionMinor)
+	binary.BigEndian.PutUint32(data[8:], VersionPatch)
+
+	// Transmit the bytes.
+	_, err := writer.Write(data[:])
+	return err
+}
+
+func ReceiveVersion(reader io.Reader) (uint32, uint32, uint32, error) {
+	// Read the bytes.
+	var data versionBytes
+	if _, err := io.ReadFull(reader, data[:]); err != nil {
+		return 0, 0, 0, err
+	}
+
+	// Decode components.
+	major := binary.BigEndian.Uint32(data[:4])
+	minor := binary.BigEndian.Uint32(data[4:8])
+	patch := binary.BigEndian.Uint32(data[8:])
+
+	// Done.
+	return major, minor, patch, nil
+}
+
+func ReceiveAndCompareVersion(reader io.Reader) (bool, error) {
+	// Receive the version.
+	major, minor, patch, err := ReceiveVersion(reader)
+	if err != nil {
+		return false, err
+	}
+
+	// Compare the version.
+	return major == VersionMajor &&
+		minor == VersionMinor &&
+		patch == VersionPatch, nil
 }
