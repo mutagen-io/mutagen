ffff85a0c218fd6256c242e57b9477aa3954b6bb
Jacob Howard <jacob@havoc.io>
2016-11-08 22:10:03 +0200

    Added license information for the gopass package.
    This commit adds license information for the gopass package
    (https://github.com/howeyc/gopass) (forked and modified at
    https://github.com/havoc-io/gopass).

--- legal.go
+++ legal.go
@@ -286,6 +286,33 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
+gopass (https://github.com/howeyc/gopass)
+
+Forked and modified at https://github.com/havoc-io/gopass
+
+Portions of this package are derived from Solaris sources distributed under the
+Common Development and Distribution License, Version 1.0. These portions are
+consequently released under the same license. Copies of the license are
+available in the above repositories. The remainder of the package is distributed
+under the ISC License.
+
+ISC License
+
+Copyright (c) 2012 Chris Howey
+
+Permission to use, copy, modify, and distribute this software for any purpose
+with or without fee is hereby granted, provided that the above copyright notice
+and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+THIS SOFTWARE.
+
+
 protobuf (https://github.com/golang/protobuf)
 
 Go support for Protocol Buffers - Google's data interchange format
