985dd39a168397a44166fcfe85c6298df983607a
Jacob Howard <jacob@havoc.io>
2016-12-08 17:57:39 +0200

    Added a nested state tracking facility for use in the session service.
    This state tracking uses a hierarchy of condition variables and indices,
    all sharing a common lock, to track changes to the global state of
    sessions, but also allows code to poll for changes to a single substate
    (in this case, a single session). The state can be arbitrarily divided
    into substates, subsubstates, and so-on, though for now we only need a
    1-level hierachy for efficient operation.

---
+++ state/state.go
@@ -0,0 +1,83 @@
+package state
+
+import (
+	"sync"
+)
+
+type State struct {
+	index  uint64
+	change *sync.Cond
+	parent *State
+}
+
+func NewState() *State {
+	return &State{
+		index:  1,
+		change: sync.NewCond(&sync.Mutex{}),
+	}
+}
+
+func (s *State) Substate() *State {
+	return &State{
+		index:  1,
+		change: sync.NewCond(s.change.L),
+		parent: s,
+	}
+}
+
+func (s *State) Lock() {
+	// Acquire the lock for the entire state hierarchy (they all share a lock).
+	s.change.L.Lock()
+}
+
+func (s *State) incrementIndexRecursive() {
+	// Increment our state index.
+	s.index += 1
+
+	// Recursively incrememnt the index of any parent states.
+	if s.parent != nil {
+		s.parent.incrementIndexRecursive()
+	}
+}
+
+func (s *State) broadcastRecursive() {
+	// Broadcast to any listeners watching this state.
+	s.change.Broadcast()
+
+	// Recursively broadcast to any listeners watching any parent states.
+	if s.parent != nil {
+		s.parent.broadcastRecursive()
+	}
+}
+
+func (s *State) NotifyOfChangesAndUnlock() {
+	// Incrememnt the index for the entire state hierarchy.
+	s.incrementIndexRecursive()
+
+	// Broadcast notifications up the state hierarchy.
+	s.broadcastRecursive()
+
+	// Release the lock for the entire state hierarchy (they all share a lock).
+	s.change.L.Unlock()
+}
+
+// TODO: Document that callers should pass 0 if they have no previous state
+// index.
+func (s *State) WaitForChangeAndLock(previousIndex uint64) uint64 {
+	// Acquire the lock for the entire state hierarchy (they all share a lock).
+	s.change.L.Lock()
+
+	// Wait until there is a change to the index for this state. Any substates
+	// that change will also recursively update their parent states' indexes.
+	for s.index == previousIndex {
+		s.change.Wait()
+	}
+
+	// Return the new index. We'll still own the lock at this point.
+	return s.index
+}
+
+func (s *State) Unlock() {
+	// Release the lock for the entire state hierarchy (they all share a lock).
+	s.change.L.Unlock()
+}
