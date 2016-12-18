aa7c1a6d7f690591f5d14ef657f81c97cfaecacd
Jacob Howard <jacob@havoc.io>
2016-12-18 20:23:36 +0200

    Changed executability functions to mutate copies, added stripping.
    This commit modifies PropagateExecutability to operate on a copy of the
    snapshot (like Apply and Transform). This particular algorithm could
    probably be remade to only allocate on changes, but it'll take some
    thought (it's hard to allocate a contents slice if only some element in
    the slice changes, but should be possible with index tracking and a
    copy). Anyway, copy operations aren't a huge time driver so we'll take
    this simple approach for now.
    
    This commit also adds a StripExecutability function that'll be necessary
    to compute expected snapshots for systems that don't support
    executability propagation.

--- sync/executability.go
+++ sync/executability.go
@@ -1,8 +1,8 @@
 package sync
 
-func PropagateExecutability(ancestor, snapshot *Entry) {
-	// If either entry is nil or their kinds don't match, there's nothing to
-	// propagate.
+func propagateExecutability(ancestor, snapshot *Entry) {
+	// If either entry is nil or their types don't match, then there's nothing
+	// to propagate.
 	if ancestor == nil || snapshot == nil || ancestor.Kind != snapshot.Kind {
 		return
 	}
@@ -13,10 +13,50 @@ func PropagateExecutability(ancestor, snapshot *Entry) {
 	} else if snapshot.Kind == EntryKind_Directory {
 		for _, sc := range snapshot.Contents {
 			if a, ok := ancestor.Find(sc.Name); ok {
-				PropagateExecutability(a, sc.Entry)
+				propagateExecutability(a, sc.Entry)
 			}
 		}
 	} else {
 		panic("unhandled entry kind")
 	}
+}
+
+func PropagateExecutability(ancestor, snapshot *Entry) *Entry {
+	// Create a copy of the snapshot that we can mutate.
+	result := snapshot.copy()
+
+	// Perform propagation.
+	propagateExecutability(ancestor, result)
+
+	// Done.
+	return result
+}
+
+func stripExecutability(snapshot *Entry) {
+	// If the entry is nil, then there's nothing to strip.
+	if snapshot == nil {
+		return
+	}
+
+	// Handle the propagation based on entry kind.
+	if snapshot.Kind == EntryKind_File {
+		snapshot.Executable = false
+	} else if snapshot.Kind == EntryKind_Directory {
+		for _, sc := range snapshot.Contents {
+			stripExecutability(sc.Entry)
+		}
+	} else {
+		panic("unhandled entry kind")
+	}
+}
+
+func StripExecutability(snapshot *Entry) *Entry {
+	// Create a copy of the snapshot that we can mutate.
+	result := snapshot.copy()
+
+	// Perform stripping.
+	stripExecutability(result)
+
+	// Done.
+	return result
 }
