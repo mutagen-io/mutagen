0dcf2acfb1af8d525701a2155d4123ab14f0cee9
Jacob Howard <jacob@havoc.io>
2016-12-22 02:05:49 +0200

    Reinstated agent side of version handshake.
    Somehow this got lost in the shuffle away from gRPC.

--- cmd/mutagen-agent/main.go
+++ cmd/mutagen-agent/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/pkg/errors"
 
+	"github.com/havoc-io/mutagen"
 	"github.com/havoc-io/mutagen/agent"
 	"github.com/havoc-io/mutagen/cmd"
 	"github.com/havoc-io/mutagen/session"
@@ -20,20 +21,29 @@ func main() {
 	flagSet := cmd.NewFlagSet("mutagen-agent", agentUsage, []int{1})
 	mode := flagSet.ParseOrDie(os.Args[1:])[0]
 
-	// Handle based on mode.
+	// Handle install.
 	if mode == agent.ModeInstall {
-		// Invoke installation.
 		if err := agent.Install(); err != nil {
 			cmd.Fatal(errors.Wrap(err, "unable to install"))
 		}
-	} else if mode == agent.ModeEndpoint {
+		return
+	}
+
+	// Create a stream on standard input/output.
+	stdio := stream.New(os.Stdin, os.Stdout, os.Stdout)
+
+	// Perform a handshake.
+	if err := mutagen.SendVersion(stdio); err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to transmit version"))
+	}
+
+	// Handle based on mode.
+	if mode == agent.ModeEndpoint {
 		// Serve an endpoint on standard input/output and monitor for its
 		// termination.
 		endpointTermination := make(chan error, 1)
 		go func() {
-			endpointTermination <- session.ServeEndpoint(
-				stream.New(os.Stdin, os.Stdout, os.Stdout),
-			)
+			endpointTermination <- session.ServeEndpoint(stdio)
 		}()
 
 		// Wait for termination from a signal or the endpoint.
