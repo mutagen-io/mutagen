47a055fd4672c069813741e099278933b0125281
Jacob Howard <jacob@havoc.io>
2017-01-02 20:20:40 +0200

    Removed JSON encoding routines.
    These weren't used and also had some semantics that didn't quite jive
    with their Protocol Buffers equivalents.

--- encoding/protobuf.go
+++ encoding/protobuf.go
@@ -4,6 +4,12 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
+// TODO: Now that this package no longer aims to support JSON encoding, it might
+// make sense to flatten these functions and relocate them to the session
+// package, which is the only place that they're used. But I can see us using
+// them in other packages, so I'll let this idea simmer, and we might even bring
+// JSON back for other purposes.
+
 func LoadAndUnmarshalProtobuf(path string, message proto.Message) error {
 	return loadAndUnmarshal(path, func(data []byte) error {
 		return proto.Unmarshal(data, message)
