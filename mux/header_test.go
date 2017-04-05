b81110c875cd42846f5b2f11fe7975ed277b3003
Jacob Howard <jacob@havoc.io>
2017-04-05 17:47:43 +0300

    Added a static cooperative multiplexing package.
    This package will form the basis for the new controller/endpoint
    network. It multiplexes streams into a configurable but static number of
    channels (i.e. you can't open new streams) which must behave
    cooperatively. It's far more efficient than using windowed multiplexing
    and opening a large number of channels.
    
    The current tests are more like acceptance tests than unit tests, but
    they should be more than sufficient to guarantee correctness. They don't
    cover a few error conditions, a few of which will be hard to simulate,
    but this can probably wait.

---
+++ mux/header_test.go
@@ -0,0 +1,47 @@
+package mux
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestHeaderTransport(t *testing.T) {
+	// Define test cases.
+	testCases := []header{
+		{0, 0},
+		{0, 1},
+		{0, 12345678},
+		{0, maxBlockLength},
+		{1, 0},
+		{1, 1},
+		{1, 12345678},
+		{1, maxBlockLength},
+		{100, 0},
+		{100, 1},
+		{100, 12345678},
+		{100, maxBlockLength},
+		{255, 0},
+		{255, 1},
+		{255, 12345678},
+		{255, maxBlockLength},
+	}
+
+	// Create our transport.
+	transport := &bytes.Buffer{}
+
+	// Encode them.
+	for i, c := range testCases {
+		if err := c.write(transport); err != nil {
+			t.Fatalf("unable to encode header %d: %v", i, err)
+		}
+	}
+
+	// Decode them.
+	for i, c := range testCases {
+		if decoded, err := readHeader(transport); err != nil {
+			t.Fatalf("unable to encode header %d: %v", i, err)
+		} else if decoded.channel != c.channel || decoded.length != c.length {
+			t.Error("header mismatch at index ", i)
+		}
+	}
+}
