de66189f459c41a55bf08ecda2ada51c61b1e2e2
Jacob Howard <jacob@havoc.io>
2017-04-13 00:01:37 +0300

    Added another clarifying comment.
    This should have said that there will be *fewer* map reallocations.

--- sync/iterate.go
+++ sync/iterate.go
@@ -4,7 +4,7 @@ func nameUnion(contentMaps ...map[string]*Entry) map[string]bool {
 	// Create the result. As a very rough but fast heuristic, we use the size of
 	// the first map as an estimate of the required capacity. For most cases,
 	// where all maps have the same contents due to a lack of changes, this
-	// should provide savings due to map reallocations.
+	// should provide savings due to fewer (or no) map reallocations.
 	capacity := 0
 	if len(contentMaps) > 0 {
 		capacity = len(contentMaps[0])
