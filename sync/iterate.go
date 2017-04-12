804ca19cdcdd2811e685ec5effb30a269af6f21d
Jacob Howard <jacob@havoc.io>
2017-04-12 19:09:57 +0300

    Added capacity heuristic to nameUnion.
    This heuristic isn't perfect, but it is fast and shaves off about 15-20%
    of the name union creation time in a somewhat artificial benchmark. The
    savings are at the higher end of the spectrum for cases where all sides
    agree on contents.

--- sync/iterate.go
+++ sync/iterate.go
@@ -1,8 +1,15 @@
 package sync
 
 func nameUnion(contentMaps ...map[string]*Entry) map[string]bool {
-	// Create the result.
-	result := make(map[string]bool)
+	// Create the result. As a very rough but fast heuristic, we use the size of
+	// the first map as an estimate of the required capacity. For most cases,
+	// where all maps have the same contents due to a lack of changes, this
+	// should provide savings due to map reallocations.
+	capacity := 0
+	if len(contentMaps) > 0 {
+		capacity = len(contentMaps[0])
+	}
+	result := make(map[string]bool, capacity)
 
 	// Populate it.
 	for _, contents := range contentMaps {
