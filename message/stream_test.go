b1f2dfa255238377ab0acd4b461077b2ba6a4bcf
Jacob Howard <jacob@havoc.io>
2017-07-25 15:17:32 +0300

    Added basic tests for message package.
    This test was mostly to satisfy our own curiosity about gob encoding,
    but it also provides a decent smoke test for the message package.

---
+++ message/stream_test.go
@@ -0,0 +1,35 @@
+package message
+
+import (
+	"bytes"
+	"testing"
+)
+
+type sampleMessage struct {
+	Message string
+	Count   int
+}
+
+func TestStreamClean(t *testing.T) {
+	// Create a byte buffer to use as a transport.
+	buffer := &bytes.Buffer{}
+
+	// Wrap it in a message stream.
+	stream := NewMessageStream(buffer)
+
+	// Write a message to the stream.
+	if err := stream.Encode(sampleMessage{"content", 100}); err != nil {
+		t.Fatal("unable to encode sample message:", err)
+	}
+
+	// Read it from the stream.
+	var message sampleMessage
+	if err := stream.Decode(&message); err != nil {
+		t.Fatal("unable to decode sample message:", err)
+	}
+
+	// Verify that the transport is left clean.
+	if buffer.Len() != 0 {
+		t.Error("transport left dirty")
+	}
+}
