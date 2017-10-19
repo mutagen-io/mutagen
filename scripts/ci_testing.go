9ee0f39a824a0733d25bff3bdf8e1be67e191492
Jacob Howard <jacob@havoc.io>
2017-10-19 18:57:01 +0300

    Modified CI testing script to invoke gocovmerge.
    Windows' cmd.exe doesn't support wildcards, so we need an alternative
    mechanism for passing coverage profiles to gocovmerge. Since the
    ci_testing.go script knows the location of these files, it makes sense
    for it to just invoke gocovmerge directly, and it also gives us a chance
    to use a temporary directory to house these files.

--- scripts/ci_testing.go
+++ scripts/ci_testing.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -25,20 +27,36 @@ func main() {
 
 	// Create a list of all the packages to test. Go tooling uses "\n" rather
 	// than "\r\n", even on Windows, so we don't need to do any replace.
-	var packages []string
-	list := exec.Command("go", "list", "./...")
-	if o, err := list.Output(); err != nil {
+	listCommand := exec.Command("go", "list", "./...")
+	listOutput, err := listCommand.Output()
+	if err != nil {
 		cmd.Fatal(errors.Wrap(err, "unable to list test modules"))
-	} else {
-		packages = strings.Split(strings.TrimSpace(string(o)), "\n")
 	}
+	packages := strings.Split(strings.TrimSpace(string(listOutput)), "\n")
 
-	// Loop over packages.
+	// Create a temporary directory where we can place coverage profiles and
+	// register its cleanup on a best-effort basis (if we die before it's
+	// removed, then the OS will deal with it).
+	temporaryDirectory, err := ioutil.TempDir("", "coverage")
+	if err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to create temporary directory"))
+	}
+	defer os.RemoveAll(temporaryDirectory)
+
+	// Loop over packages and execute tests.
+	var coverageProfiles []string
 	for _, p := range packages {
 		// Skip packages that aren't really packages.
 		if p == "github.com/havoc-io/mutagen/scripts" {
 			continue
 		}
+
+		// Create a unique name for the coverage profile for this package.
+		profile := filepath.Join(
+			temporaryDirectory,
+			fmt.Sprintf("%s.cover", uuid.NewV4().String()),
+		)
+		coverageProfiles = append(coverageProfiles, profile)
 
 		// Set up arguments for testing.
 		testArguments := []string{"test", "-v"}
@@ -47,16 +65,44 @@ func main() {
 		}
 		testArguments = append(
 			testArguments,
-			fmt.Sprintf("-coverprofile=%s.cover", uuid.NewV4().String()),
+			fmt.Sprintf("-coverprofile=%s", profile),
 			p,
 		)
 
 		// Execute tests for the package.
-		tests := exec.Command("go", testArguments...)
-		tests.Stdout = os.Stdout
-		tests.Stderr = os.Stderr
-		if err := tests.Run(); err != nil {
+		testCommand := exec.Command("go", testArguments...)
+		testCommand.Stdout = os.Stdout
+		testCommand.Stderr = os.Stderr
+		if err := testCommand.Run(); err != nil {
 			cmd.Fatal(errors.Wrap(err, "unable to execute tests"))
 		}
+	}
+
+	// Not all packages have tests, so not all coverage profiles will actually
+	// be generated. Filter out those that don't exist.
+	var extantCoverageProfiles []string
+	for _, p := range coverageProfiles {
+		if _, err = os.Stat(p); err != nil {
+			if os.IsNotExist(err) {
+				continue
+			} else {
+				cmd.Fatal(errors.Wrap(err, "unable to provide coverage profile"))
+			}
+		} else {
+			extantCoverageProfiles = append(extantCoverageProfiles, p)
+		}
+	}
+
+	// Invoke gocovmerge to create a combined coverage profile.
+	combinedProfile, err := os.Create("coverage.txt")
+	if err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to create merged coverage profile"))
+	}
+	defer combinedProfile.Close()
+	mergeCommand := exec.Command("gocovmerge", extantCoverageProfiles...)
+	mergeCommand.Stdout = combinedProfile
+	mergeCommand.Stderr = os.Stderr
+	if err := mergeCommand.Run(); err != nil {
+		cmd.Fatal(errors.Wrap(err, "unable to merge coverge profiles"))
 	}
 }
