08f178c986aec5a880e4671b6d796147a5dfcaa8
Jacob Howard <jacob@havoc.io>
2017-10-21 20:39:34 +0300

    Switched to using os.Executable for executable path detection.
    This has been available since Go 1.8, but it's been flakey (read:
    effectively broken due to the lack of /proc) on OpenBSD. This was fixed
    in Go 1.9, so we can finally use it.
    
    This also negates the need for the github.com/kardianos/osext package,
    which I've removed from vendoring and license information.

--- process/current.go
+++ process/current.go
@@ -1,7 +1,8 @@
 package process
 
 import (
-	"github.com/kardianos/osext"
+	"os"
+	"path/filepath"
 
 	"github.com/pkg/errors"
 )
@@ -13,22 +14,11 @@ var Current struct {
 }
 
 func init() {
-	// Compute the current executable's path.
-	// TODO: In Go 1.9, os.Executable will be fixed for OpenBSD (it will do what
-	// osext does at the moment), so switch to that and remove the osext
-	// dependency, licensing, and vendored code.
-	if path, err := osext.Executable(); err != nil {
+	// Compute the current executable's path and parent path.
+	if path, err := os.Executable(); err != nil {
 		panic(errors.Wrap(err, "unable to compute current executable's path"))
 	} else {
 		Current.ExecutablePath = path
-	}
-
-	// Compute the current executable's parent path.
-	// TODO: In Go 1.9, switch to just taking the parent directory of what's
-	// returned by os.Executable. That's exactly what osext does at the moment.
-	if path, err := osext.ExecutableFolder(); err != nil {
-		panic(errors.Wrap(err, "unable to compute current executable's parent path"))
-	} else {
-		Current.ExecutableParentPath = path
+		Current.ExecutableParentPath = filepath.Dir(path)
 	}
 }
