4e318a4a3a0049750b2ad24bedd9affb0be5e87f
Jacob Howard <jacob@havoc.io>
2017-11-10 01:29:19 +0200

    Fixed message package tests.
    Some of these were still using compression. I missed these in the
    previous commit.

--- message/stream_test.go
+++ message/stream_test.go
@@ -11,15 +11,12 @@ type sampleMessage struct {
 	Count   int
 }
 
-func runStreamClean(t *testing.T, compressed bool) {
-	// Mark this as a helper function.
-	t.Helper()
-
+func TestStreamClean(t *testing.T) {
 	// Create a byte buffer to use as a transport.
 	buffer := &bytes.Buffer{}
 
 	// Wrap it in a message stream.
-	stream := NewStream(buffer, compressed)
+	stream := NewStream(buffer)
 
 	// Write a message to the stream.
 	if err := stream.Encode(sampleMessage{"content", 100}); err != nil {
@@ -38,14 +35,6 @@ func runStreamClean(t *testing.T, compressed bool) {
 	}
 }
 
-func TestStreamClean(t *testing.T) {
-	runStreamClean(t, false)
-}
-
-func TestCompressedStreamClean(t *testing.T) {
-	runStreamClean(t, true)
-}
-
 type badReadWriter struct{}
 
 func (r *badReadWriter) Read(_ []byte) (int, error) {
@@ -61,7 +50,7 @@ func TestEOFDecode(t *testing.T) {
 	connection := &badReadWriter{}
 
 	// Create a message stream.
-	stream := NewStream(connection, false)
+	stream := NewStream(connection)
 
 	// Ensure that the decode fails and returns the EOF error unwrapped.
 	var message sampleMessage
@@ -74,26 +63,8 @@ func TestEncodeFail(t *testing.T) {
 	// Create a bad connection.
 	connection := &badReadWriter{}
 
-	// Create a message stream without compression so that we can check for
-	// failures encoding to the underlying connection.
-	stream := NewStream(connection, false)
-
-	// Ensure that the encode fails and returns the ErrClosedPipe error
-	// unwrapped.
-	var message sampleMessage
-	if err := stream.Encode(&message); err != io.ErrClosedPipe {
-		t.Error("expected ErrClosedPipe from closed connection")
-	}
-}
-
-func TestEncodeFailOnFlush(t *testing.T) {
-	// Create a bad connection.
-	connection := &badReadWriter{}
-
-	// Create a message stream with compression so that we can check for
-	// failures flushing to the underlying connection. This assumes that the
-	// compressors buffer is large enough not to flush during the encode.
-	stream := NewStream(connection, true)
+	// Create a message stream.
+	stream := NewStream(connection)
 
 	// Ensure that the encode fails and returns the ErrClosedPipe error
 	// unwrapped.
