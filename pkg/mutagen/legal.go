41afcda6e3bda50afb8696b5a1a02f65cc64da06
Jacob Howard <jacob@havoc.io>
2018-05-14 23:11:30 +0300

    Vendored mousestrap package and added license information.
    This commit vendors the inconshreveable/mousetrap package and adds its
    license information. This package is a dependency of Cobra, but it was
    only apparent on Windows.

--- pkg/mutagen/legal.go
+++ pkg/mutagen/legal.go
@@ -325,6 +325,23 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
+mousestrap (https://github.com/inconshreveable/mousetrap)
+
+Copyright 2014 Alan Shreve
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
 Go support for Protocol Buffers (https://github.com/golang/protobuf)
 
 Copyright 2010 The Go Authors.  All rights reserved.
