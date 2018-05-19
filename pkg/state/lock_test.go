f8f8481e6d7da45d4c507715e07b9ea5a4e0cf3e
Jacob Howard <jacob@havoc.io>
2018-05-19 15:36:53 +0300

    Implemented tests for the state package.
    These are theoretically flaky since they rely on mutex/condition
    variable/scheduler timescales being below a certain time threshold, but
    I think the threshold is reasonably conservative without eating up
    testing time. It's sort of a necessary evil for this package.

--- pkg/state/lock_test.go
+++ pkg/state/lock_test.go
@@ -1,3 +1,70 @@
 package state
 
-// TODO: Implement.
+import (
+	"testing"
+	"time"
+)
+
+func TestTrackingLock(t *testing.T) {
+	// Create a tracker.
+	tracker := NewTracker()
+
+	// Wrap it in a tracking lock.
+	lock := NewTrackingLock(tracker)
+
+	// Create a channel for Goroutine communication.
+	handoff := make(chan bool)
+
+	// Start a Goroutine with which we'll coordinate.
+	go func() {
+		// Wait for a successful change.
+		firstState, poisoned := tracker.WaitForChange(0)
+		if poisoned || firstState == 0 {
+			handoff <- false
+			return
+		}
+		handoff <- true
+
+		// Wait for poisoning.
+		_, poisoned = tracker.WaitForChange(firstState)
+		handoff <- poisoned
+	}()
+
+	// Acquire and release the lock in a way that will change the state, and
+	// then wait for a response.
+	lock.Lock()
+	lock.Unlock()
+	select {
+	case value := <-handoff:
+		if !value {
+			t.Fatal("received failure on state tracking")
+		}
+	case <-time.After(trackerTestTimeout):
+		t.Fatal("timeout failure on state tracking")
+	}
+
+	// Sleep for enough time that the Goroutine can invoke the condition
+	// variable wait.
+	time.Sleep(trackerTestSleep)
+
+	// Acquire and release the lock in a way that won't change the state. We
+	// don't expect a response here, but our poison response will be invalid if
+	// this does change the state.
+	lock.Lock()
+	lock.UnlockWithoutNotify()
+
+	// Sleep for enough time that the Goroutine can invoke the condition
+	// variable wait.
+	time.Sleep(trackerTestSleep)
+
+	// Poison the tracker and wait for a response.
+	tracker.Poison()
+	select {
+	case value := <-handoff:
+		if !value {
+			t.Fatal("received failure on state poisoning")
+		}
+	case <-time.After(trackerTestTimeout):
+		t.Fatal("timeout failure on state poisoning")
+	}
+}
--- pkg/state/tracker_test.go
+++ pkg/state/tracker_test.go
@@ -1,3 +1,60 @@
 package state
 
-// TODO: Implement.
+import (
+	"testing"
+	"time"
+)
+
+const (
+	trackerTestSleep   = 10 * time.Millisecond
+	trackerTestTimeout = 1 * time.Second
+)
+
+func TestTracker(t *testing.T) {
+	// Create a tracker.
+	tracker := NewTracker()
+
+	// Create a channel for Goroutine communication.
+	handoff := make(chan bool)
+
+	// Start a Goroutine with which we'll coordinate.
+	go func() {
+		// Wait for a successful change.
+		firstState, poisoned := tracker.WaitForChange(0)
+		if poisoned || firstState == 0 {
+			handoff <- false
+			return
+		}
+		handoff <- true
+
+		// Wait for poisoning.
+		_, poisoned = tracker.WaitForChange(firstState)
+		handoff <- poisoned
+	}()
+
+	// Notify of a change and wait for a response.
+	tracker.NotifyOfChange()
+	select {
+	case value := <-handoff:
+		if !value {
+			t.Fatal("received failure on state tracking")
+		}
+	case <-time.After(trackerTestTimeout):
+		t.Fatal("timeout failure on state tracking")
+	}
+
+	// Sleep for enough time that the Goroutine can invoke the condition
+	// variable wait.
+	time.Sleep(trackerTestSleep)
+
+	// Poison the tracker and wait for a response.
+	tracker.Poison()
+	select {
+	case value := <-handoff:
+		if !value {
+			t.Fatal("received failure on state poisoning")
+		}
+	case <-time.After(trackerTestTimeout):
+		t.Fatal("timeout failure on state poisoning")
+	}
+}
