35eb0683f54151836fd9085167f9c221ceb40f75
Jacob Howard <jacob@havoc.io>
2018-05-19 15:18:26 +0300

    Added tests for URL validation.
    This commit adds sanity tests for URL structure validation code.

--- pkg/url/url_test.go
+++ pkg/url/url_test.go
@@ -1,3 +1,57 @@
 package url
 
-// TODO: Implement.
+import (
+	"testing"
+)
+
+func TestProtocolSupported(t *testing.T) {
+	// Ensure that the local protocol is marked as supported.
+	if !Protocol_Local.supported() {
+		t.Error("local protocol marked as unsupported")
+	}
+
+	// Ensure that the SSH protocol is marked as supported.
+	if !Protocol_SSH.supported() {
+		t.Error("SSH protocol marked as unsupported")
+	}
+
+	// Ensure that one above the highest value protocol is marked as
+	// unsupported. This should help keep enumeration values and test code in
+	// sync.
+	if (Protocol_SSH+1).supported() {
+		t.Error("unknown protocol marked as supported")
+	}
+}
+
+func TestURLEnsureValid(t *testing.T) {
+	// Ensure that a nil URL is invalid.
+	var invalid *URL
+	if invalid.EnsureValid() == nil {
+		t.Error("nil URL marked as valid")
+	}
+
+	// Ensure that a URL with an unsupported protocol is invalid. This should
+	// also keep enumeration values and test code in sync.
+	invalid = &URL{
+		Protocol: (Protocol_SSH+1),
+		Username: "george",
+		Hostname: "washington",
+		Port: 22,
+		Path: "~/path",
+	}
+	if invalid.EnsureValid() == nil {
+		t.Error("nil URL marked as valid")
+	}
+
+	// Ensure that a sane URL is valid.
+	valid := &URL{
+		Protocol: Protocol_SSH,
+		Username: "george",
+		Hostname: "washington",
+		Port: 22,
+		Path: "~/path",
+	}
+	if err := valid.EnsureValid(); err != nil {
+		t.Error("valid URL marked as invalid:", err)
+	}
+}
