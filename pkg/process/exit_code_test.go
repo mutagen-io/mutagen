d03b393830a08476dbafa97a80b47325692f66d5
Jacob Howard <jacob@havoc.io>
2018-05-19 15:52:01 +0300

    Added tests for the process package.
    Unfortunately not every code path in this package lends itself to
    testing, because some of it relies on syscall/os package internals that
    we can't get at. But the coverage is now at 90%, which I can live with.

--- pkg/process/exit_code_test.go
+++ pkg/process/exit_code_test.go
@@ -1,3 +1,37 @@
 package process
 
-// TODO: Implement.
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestExitCodeForNilError(t *testing.T) {
+	if _, err := ExitCodeForError(nil); err == nil {
+		t.Error("exit code was returned for nil error")
+	}
+}
+
+func TestExitCodeForInvalidError(t *testing.T) {
+	if _, err := ExitCodeForError(errors.New("not an exec error")); err == nil {
+		t.Error("exit code was returned for invalid error")
+	}
+}
+
+// TODO: It doesn't seem like there's anyway to test extraction of the
+// syscall.WaitStatus from the error, because we can't construct an
+// os.ProcessState (and it's not documented that we can rely on its zero value).
+// Maybe look into this further?
+
+func TestExitCode(t *testing.T) {
+	// Run "go mutagen-test-invalid", which should return an error code of 2,
+	// and verify its exit code.
+	if err := exec.Command("go", "mutagen-test-invalid").Run(); err == nil {
+		t.Fatal("expected non-nil error when running invalid Go command")
+	} else if code, codeErr := ExitCodeForError(err); codeErr != nil {
+		t.Fatal("unable to extract error exit code:", codeErr)
+	} else if code != 2 {
+		t.Error("exit code did not match expected")
+	}
+}
--- pkg/process/name_test.go
+++ pkg/process/name_test.go
@@ -1,3 +1,17 @@
 package process
 
-// TODO: Implement.
+import (
+	"testing"
+)
+
+func TestExecutableNameWindows(t *testing.T) {
+	if name := ExecutableName("mutagen-agent", "windows"); name != "mutagen-agent.exe" {
+		t.Error("executable name incorrect for Windows")
+	}
+}
+
+func TestExecutableNameLinux(t *testing.T) {
+	if name := ExecutableName("mutagen-agent", "linux"); name != "mutagen-agent" {
+		t.Error("executable name incorrect for Linux")
+	}
+}
