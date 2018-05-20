4f569c2a560e0e12f4869da7ad782a94b7c60e0c
Jacob Howard <jacob@havoc.io>
2018-05-20 17:59:30 +0300

    Implemented some unit tests for the prompt package.
    These tests don't test command line prompting, but I don't know if
    that's easily possible.

--- pkg/prompt/classify_test.go
+++ pkg/prompt/classify_test.go
@@ -1,3 +1,23 @@
 package prompt
 
-// TODO: Implement.
+import (
+	"testing"
+)
+
+func TestClassifyBinaryQuestionMark(t *testing.T) {
+	if k := Classify("Question? (yes/no)? "); k != PromptKindBinary {
+		t.Error("misclassified binary prompt as", k)
+	}
+}
+
+func TestClassifyBinaryQuestionColon(t *testing.T) {
+	if k := Classify("Question? (yes/no): "); k != PromptKindBinary {
+		t.Error("misclassified binary prompt as", k)
+	}
+}
+
+func TestClassifySecret(t *testing.T) {
+	if k := Classify("Give me your password: "); k != PromptKindSecret {
+		t.Error("misclassified secret prompt as", k)
+	}
+}
---
+++ pkg/prompt/command_line_test.go
@@ -0,0 +1,9 @@
+package prompt
+
+// TODO: Figure out a way to test command line prompting. I'm not sure that this
+// is even possible. We might be able to swap out the OS interfaces that gopass
+// is calling down to, but it would be tricky in a parallel testing environment.
+// Alternatively, we could restructure the function to more easily test parts of
+// it, but that's just a bit pointless. The gopass package is so well-tested,
+// including with tests we wrote, and the wrapper function is so simple, that I
+// think we probably don't need to bother.
