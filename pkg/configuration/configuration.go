702bede109088b5882b5405577a3d253b3f51ce6
Jacob Howard <jacob@havoc.io>
2018-05-20 17:50:12 +0300

    Implemented tests for the configuration package.
    This commit adds unit tests for the configuration package. A small
    amount of code restructuring was necessary to enable full test coverage.

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -13,11 +13,9 @@ type Configuration struct {
 	} `toml:"ignore"`
 }
 
-// Load loads the Mutagen configuration file from disk and populates a
-// Configuration structure. If the Mutagen configuration file does not exist,
-// this method will return a structure with the default configuration values.
-// The returned structure is not re-used, so its members can be freely mutated.
-func Load() (*Configuration, error) {
+// loadFromPath is the internal loading function. We keep it separate from Load
+// so that we can get full test coverage using temporary files.
+func loadFromPath(path string) (*Configuration, error) {
 	// Create a configuration that we can decode into. We set any default values
 	// here because nothing will be modified in this structure if the
 	// configuration file doesn't exist.
@@ -26,7 +24,7 @@ func Load() (*Configuration, error) {
 	// Attempt to load the configuration from disk.
 	// TODO: Should we implement a caching mechanism where we run a stat call
 	// and watch for filesystem modification?
-	if err := encoding.LoadAndUnmarshalTOML(filesystem.MutagenConfigurationPath, result); err != nil {
+	if err := encoding.LoadAndUnmarshalTOML(path, result); err != nil {
 		if !os.IsNotExist(err) {
 			return nil, err
 		}
@@ -34,4 +32,12 @@ func Load() (*Configuration, error) {
 
 	// Return the configuration.
 	return result, nil
+}
+
+// Load loads the Mutagen configuration file from disk and populates a
+// Configuration structure. If the Mutagen configuration file does not exist,
+// this method will return a structure with the default configuration values.
+// The returned structure is not re-used, so its members can be freely mutated.
+func Load() (*Configuration, error) {
+	return loadFromPath(filesystem.MutagenConfigurationPath)
 }
--- pkg/configuration/configuration_test.go
+++ pkg/configuration/configuration_test.go
@@ -1,3 +1,101 @@
 package configuration
 
-// TODO: Implement.
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+const (
+	testConfigurationGibberish = "[a+1a4"
+	testConfigurationValid     = `[ignore]
+default = ["ignore/this/**", "!ignore/this/that"]
+`
+)
+
+func TestLoadNonExistent(t *testing.T) {
+	if c, err := loadFromPath("/this/does/not/exist"); err != nil {
+		t.Error("load from non-existent path failed:", err)
+	} else if c == nil {
+		t.Error("load from non-existent path returned nil configuration")
+	}
+}
+
+func TestLoadEmpty(t *testing.T) {
+	// Create an empty temporary file and defer its cleanup.
+	file, err := ioutil.TempFile("", "mutagen_configuration")
+	if err != nil {
+		t.Error("unable to create temporary file:", err)
+	} else if err = file.Close(); err != nil {
+		t.Error("unable to close temporary file:", err)
+	}
+	defer os.Remove(file.Name())
+
+	// Attempt to load.
+	if c, err := loadFromPath(file.Name()); err != nil {
+		t.Error("load from empty file failed:", err)
+	} else if c == nil {
+		t.Error("load from empty file returned nil configuration")
+	}
+}
+
+func TestLoadGibberish(t *testing.T) {
+	// Write gibberish to a temporary file and defer its cleanup.
+	file, err := ioutil.TempFile("", "mutagen_configuration")
+	if err != nil {
+		t.Fatal("unable to create temporary file:", err)
+	} else if _, err = file.Write([]byte(testConfigurationGibberish)); err != nil {
+		t.Fatal("unable to write data to temporary file:", err)
+	} else if err = file.Close(); err != nil {
+		t.Fatal("unable to close temporary file:", err)
+	}
+	defer os.Remove(file.Name())
+
+	// Attempt to load.
+	if _, err := loadFromPath(file.Name()); err == nil {
+		t.Error("load did not fail on gibberish configuration")
+	}
+}
+
+func TestLoadDirectory(t *testing.T) {
+	// Create a temporary directory and defer its cleanup.
+	directory, err := ioutil.TempDir("", "mutagen_configuration")
+	if err != nil {
+		t.Fatal("unable to create temporary directory:", err)
+	}
+	defer os.RemoveAll(directory)
+
+	// Attempt to load.
+	if _, err := loadFromPath(directory); err == nil {
+		t.Error("load did not fail on directory path")
+	}
+}
+
+func TestLoadValidConfiguration(t *testing.T) {
+	// Write a valid configuration to a temporary file and defer its cleanup.
+	file, err := ioutil.TempFile("", "mutagen_configuration")
+	if err != nil {
+		t.Fatal("unable to create temporary file:", err)
+	} else if _, err = file.Write([]byte(testConfigurationValid)); err != nil {
+		t.Fatal("unable to write data to temporary file:", err)
+	} else if err = file.Close(); err != nil {
+		t.Fatal("unable to close temporary file:", err)
+	}
+	defer os.Remove(file.Name())
+
+	// Attempt to load.
+	if c, err := loadFromPath(file.Name()); err != nil {
+		t.Error("load from valid configuration failed:", err)
+	} else if c == nil {
+		t.Error("load from valid configuration returned nil configuration")
+	}
+}
+
+// NOTE: This test depends on not having an invalid ~/.mutagen.toml file.
+func TestLoad(t *testing.T) {
+	if c, err := Load(); err != nil {
+		t.Error("load failed:", err)
+	} else if c == nil {
+		t.Error("load returned nil configuration")
+	}
+}
