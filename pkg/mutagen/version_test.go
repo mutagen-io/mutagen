04fd0f2d89fa1d2e5524922ec62d2b34aad2821f
Jacob Howard <jacob@havoc.io>
2018-06-06 10:10:25 +0300

    Added tests for version send/receive/compare.
    This commit adds tests for the version send/receive/compare utility
    functions.

--- pkg/mutagen/version_test.go
+++ pkg/mutagen/version_test.go
@@ -1,3 +1,42 @@
 package mutagen
 
-// TODO: Implement.
+import (
+	"bytes"
+	"testing"
+)
+
+func TestVersionSendReceiveAndCompare(t *testing.T) {
+	// Create an intermediate buffer.
+	buffer := &bytes.Buffer{}
+
+	// Send the version.
+	if err := SendVersion(buffer); err != nil {
+		t.Fatal("unable to send version:", err)
+	}
+
+	// Ensure that the buffer is non-empty.
+	if buffer.Len() != 12 {
+		t.Fatal("buffer does not contain expected byte count")
+	}
+
+	// Receive the version.
+	if match, err := ReceiveAndCompareVersion(buffer); err != nil {
+		t.Fatal("unable to receive version:", err)
+	} else if !match {
+		t.Error("version mismatch on receive")
+	}
+}
+
+func TestVersionReceiveAndCompareEmptyBuffer(t *testing.T) {
+	// Create an empty buffer.
+	buffer := &bytes.Buffer{}
+
+	// Attempt to receive the version.
+	match, err := ReceiveAndCompareVersion(buffer)
+	if err == nil {
+		t.Error("version received from empty buffer")
+	}
+	if match {
+		t.Error("version match on empty buffer")
+	}
+}
