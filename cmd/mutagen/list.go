dd19b87fa35a109745bec520f2780e4bb202046a
Jacob Howard <jacob@havoc.io>
2018-06-07 18:04:03 +0300

    Unified shared code between list and monitor.
    The session printing code was almost identical between these two, so
    I've extracted it into a standalone function.

--- cmd/mutagen/list.go
+++ cmd/mutagen/list.go
@@ -11,7 +11,6 @@ import (
 	"github.com/fatih/color"
 
 	"github.com/havoc-io/mutagen/cmd"
-	"github.com/havoc-io/mutagen/pkg/filesystem"
 	sessionpkg "github.com/havoc-io/mutagen/pkg/session"
 	sessionsvcpkg "github.com/havoc-io/mutagen/pkg/session/service"
 	"github.com/havoc-io/mutagen/pkg/sync"
@@ -22,71 +21,6 @@ func formatPath(path string) string {
 		return "(root)"
 	}
 	return path
-}
-
-func printSession(state *sessionpkg.State) {
-	// Print the session identifier.
-	fmt.Println("Session:", state.Session.Identifier)
-
-	// Print status.
-	statusString := state.Status.Description()
-	if state.Session.Paused {
-		statusString = color.YellowString("[Paused]")
-	}
-	fmt.Println("Status:", statusString)
-
-	// Print extended information, if desired.
-	if listConfiguration.long {
-		// Print default and per-session ignores.
-		if len(state.Session.GlobalConfiguration.Ignores) > 0 {
-			fmt.Println("Default ignores:")
-			for _, p := range state.Session.GlobalConfiguration.Ignores {
-				fmt.Printf("\t%s\n", p)
-			}
-		}
-		if len(state.Session.Configuration.Ignores) > 0 {
-			fmt.Println("Ignores:")
-			for _, p := range state.Session.Configuration.Ignores {
-				fmt.Printf("\t%s\n", p)
-			}
-		}
-
-		// Compute the merged session configuration.
-		mergedConfiguration := sessionpkg.MergeConfigurations(
-			state.Session.Configuration,
-			state.Session.GlobalConfiguration,
-		)
-
-		// Compute and print symlink mode.
-		symlinkModeDescription := mergedConfiguration.SymlinkMode.Description()
-		if mergedConfiguration.SymlinkMode == sync.SymlinkMode_Default {
-			defaultSymlinkMode := state.Session.Version.DefaultSymlinkMode()
-			symlinkModeDescription += fmt.Sprintf(" (%s)", defaultSymlinkMode.Description())
-		}
-		fmt.Println("Symlink mode:", symlinkModeDescription)
-
-		// Compute and print the watch mode.
-		watchModeDescription := mergedConfiguration.WatchMode.Description()
-		if mergedConfiguration.WatchMode == filesystem.WatchMode_Default {
-			defaultWatchMode := state.Session.Version.DefaultWatchMode()
-			watchModeDescription += fmt.Sprintf(" (%s)", defaultWatchMode.Description())
-		}
-		fmt.Println("Watch mode:", watchModeDescription)
-
-		// Compute and print the polling interval.
-		var watchPollingIntervalDescription string
-		if mergedConfiguration.WatchPollingInterval == 0 {
-			watchPollingIntervalDescription = fmt.Sprintf("Default (%d seconds)", filesystem.DefaultPollingInterval)
-		} else {
-			watchPollingIntervalDescription = fmt.Sprintf("%d seconds", mergedConfiguration.WatchPollingInterval)
-		}
-		fmt.Println("Watch polling interval:", watchPollingIntervalDescription)
-	}
-
-	// Print the last error, if any.
-	if state.LastError != "" {
-		color.Red("Last error: %s\n", state.LastError)
-	}
 }
 
 func formatConnectionStatus(connected bool) string {
@@ -128,6 +62,20 @@ func printEndpoint(state *sessionpkg.State, alpha bool) {
 		for _, p := range problems {
 			color.Red("\t\t%s: %v\n", formatPath(p.Path), p.Error)
 		}
+	}
+}
+
+func printSessionStatus(state *sessionpkg.State) {
+	// Print status.
+	statusString := state.Status.Description()
+	if state.Session.Paused {
+		statusString = color.YellowString("[Paused]")
+	}
+	fmt.Println("Status:", statusString)
+
+	// Print the last error, if any.
+	if state.LastError != "" {
+		color.Red("Last error: %s\n", state.LastError)
 	}
 }
 
@@ -216,9 +164,10 @@ func listMain(command *cobra.Command, arguments []string) error {
 		if printDelimiters {
 			fmt.Println(delimiterLine)
 		}
-		printSession(state)
+		printSession(state, listConfiguration.long)
 		printEndpoint(state, true)
 		printEndpoint(state, false)
+		printSessionStatus(state)
 		if len(state.Conflicts) > 0 {
 			printConflicts(state.Conflicts)
 		}
--- cmd/mutagen/monitor.go
+++ cmd/mutagen/monitor.go
@@ -11,10 +11,8 @@ import (
 	"github.com/fatih/color"
 
 	"github.com/havoc-io/mutagen/cmd"
-	"github.com/havoc-io/mutagen/pkg/filesystem"
 	sessionpkg "github.com/havoc-io/mutagen/pkg/session"
 	sessionsvcpkg "github.com/havoc-io/mutagen/pkg/session/service"
-	"github.com/havoc-io/mutagen/pkg/sync"
 )
 
 func printMonitorLine(state *sessionpkg.State) {
@@ -146,56 +144,8 @@ func monitorMain(command *cobra.Command, arguments []string) error {
 
 		// Print session information the first time through the loop.
 		if !sessionInformationPrinted {
-			// Print the session identifier.
-			fmt.Println("Session:", state.Session.Identifier)
-
-			// Print extended information, if desired.
-			if monitorConfiguration.long {
-				// Print default and per-session ignores.
-				if len(state.Session.GlobalConfiguration.Ignores) > 0 {
-					fmt.Println("Default ignores:")
-					for _, p := range state.Session.GlobalConfiguration.Ignores {
-						fmt.Printf("\t%s\n", p)
-					}
-				}
-				if len(state.Session.Configuration.Ignores) > 0 {
-					fmt.Println("Ignores:")
-					for _, p := range state.Session.Configuration.Ignores {
-						fmt.Printf("\t%s\n", p)
-					}
-				}
-
-				// Compute the merged session configuration.
-				mergedConfiguration := sessionpkg.MergeConfigurations(
-					state.Session.Configuration,
-					state.Session.GlobalConfiguration,
-				)
-
-				// Compute and print symlink mode.
-				symlinkModeDescription := mergedConfiguration.SymlinkMode.Description()
-				if mergedConfiguration.SymlinkMode == sync.SymlinkMode_Default {
-					defaultSymlinkMode := state.Session.Version.DefaultSymlinkMode()
-					symlinkModeDescription += fmt.Sprintf(" (%s)", defaultSymlinkMode.Description())
-				}
-				fmt.Println("Symlink Mode:", symlinkModeDescription)
-
-				// Compute and print the watch mode.
-				watchModeDescription := mergedConfiguration.WatchMode.Description()
-				if mergedConfiguration.WatchMode == filesystem.WatchMode_Default {
-					defaultWatchMode := state.Session.Version.DefaultWatchMode()
-					watchModeDescription += fmt.Sprintf(" (%s)", defaultWatchMode.Description())
-				}
-				fmt.Println("Watch mode:", watchModeDescription)
-
-				// Compute and print the polling interval.
-				var watchPollingIntervalDescription string
-				if mergedConfiguration.WatchPollingInterval == 0 {
-					watchPollingIntervalDescription = fmt.Sprintf("Default (%d seconds)", filesystem.DefaultPollingInterval)
-				} else {
-					watchPollingIntervalDescription = fmt.Sprintf("%d seconds", mergedConfiguration.WatchPollingInterval)
-				}
-				fmt.Println("Watch polling interval:", watchPollingIntervalDescription)
-			}
+			// Print session information.
+			printSession(state, monitorConfiguration.long)
 
 			// Print endpoint URLs.
 			fmt.Println("Alpha:", state.Session.Alpha.Format())
