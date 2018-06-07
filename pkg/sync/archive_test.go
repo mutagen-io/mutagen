c483def401473ac794b17f8735a3bf391ddeb6a7
Jacob Howard <jacob@havoc.io>
2018-06-07 20:26:50 +0300

    Added additional Archive sanity check.
    This commit adds a test to ensure that an Archive with an empty root and
    an Archive with an empty directory as its root serialize differently. If
    they didn't, it would defeat the purpose of Archive.

--- pkg/sync/archive_test.go
+++ pkg/sync/archive_test.go
@@ -7,6 +7,31 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
+func TestArchiveEmptyDifferentEmptyDirectory(t *testing.T) {
+	// Serialize an archive with a nil root. It should be an empty byte
+	// sequence.
+	emptyArchive := &Archive{}
+	emptyArchiveBytes, err := proto.Marshal(emptyArchive)
+	if err != nil {
+		t.Fatal("unable to marshal empty archive:", err)
+	}
+	if len(emptyArchiveBytes) > 0 {
+		t.Error("empty archive serialized to non-empty bytes")
+	}
+
+	// Serialize an archive with an empty directory at the root.
+	archiveEmptyDirectory := &Archive{Root:&Entry{Kind: EntryKind_Directory}}
+	archiveEmptyDirectoryBytes, err := proto.Marshal(archiveEmptyDirectory)
+	if err != nil {
+		t.Fatal("unable to marshal archive with empty directory:", err)
+	}
+
+	// Ensure they differ.
+	if bytes.Equal(emptyArchiveBytes, archiveEmptyDirectoryBytes) {
+		t.Error("empty archive and archive with empty directory serialize the same")
+	}
+}
+
 func TestArchiveConsistentSerialization(t *testing.T) {
 	// Create two entries. Although not strictly necessary, make them distinct
 	// values.
