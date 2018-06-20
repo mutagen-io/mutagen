b9df593501faad4f33e11cc95993494a64461952
Jacob Howard <jacob@havoc.io>
2018-06-20 16:38:54 +0300

    Enabled sudo on Travis and fixed SSH setup.
    We didn't require sudo previously, but we need it to set up SSH.
    
    We also apparently need to specify the -f flag to ssh-keygen - the -q
    flag by itself isn't enough to get ssh-keygen to avoid asking for a file
    path.

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -8,14 +8,17 @@ elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo service ssh restart
 fi
 
+# Ensure our SSH configuration directory exists.
+mkdir "~/.ssh"
+
 # Generate an SSH key in quiet mode without a password.
-ssh-keygen -q -t rsa -b 4096 -N ""
+ssh-keygen -q -t rsa -b 4096 -N "" -f "~/.ssh/id_rsa"
 
 # Add the key to our list of authorized keys.
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+cat "~/.ssh/id_rsa.pub" >> "~/.ssh/authorized_keys"
 
 # Add localhost to our list of known hosts (so we're not prompted).
-ssh-keyscan -t rsa localhost >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa localhost >> "~/.ssh/known_hosts"
 
 # Mark the environment as having SSH support.
 export MUTAGEN_TEST_SSH="true"
