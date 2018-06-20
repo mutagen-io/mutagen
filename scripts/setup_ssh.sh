0dc23bf02bd616163b5f0395a47bb9906ee367db
Jacob Howard <jacob@havoc.io>
2018-06-20 16:42:41 +0300

    Added a few more fixes to the SSH setup script.
    We can't quote home-relative paths, and we should exit on failure.

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -2,23 +2,23 @@
 
 # SSH installation/activation based on the OS.
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    sudo systemsetup -f -setremotelogin on
+    sudo systemsetup -f -setremotelogin on || exit $?
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sudo apt-get -qq install openssh-client openssh-server
-    sudo service ssh restart
+    sudo apt-get -qq install openssh-client openssh-server || exit $?
+    sudo service ssh restart || exit $?
 fi
 
 # Ensure our SSH configuration directory exists.
-mkdir "~/.ssh"
+mkdir ~/.ssh || exit $?
 
 # Generate an SSH key in quiet mode without a password.
-ssh-keygen -q -t rsa -b 4096 -N "" -f "~/.ssh/id_rsa"
+ssh-keygen -q -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa || exit $?
 
 # Add the key to our list of authorized keys.
-cat "~/.ssh/id_rsa.pub" >> "~/.ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys || exit $?
 
 # Add localhost to our list of known hosts (so we're not prompted).
-ssh-keyscan -t rsa localhost >> "~/.ssh/known_hosts"
+ssh-keyscan -t rsa localhost >> ~/.ssh/known_hosts || exit $?
 
 # Mark the environment as having SSH support.
 export MUTAGEN_TEST_SSH="true"
