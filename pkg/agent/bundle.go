f56d0f7d70247267864e6271024b37413149ef3a
Jacob Howard <jacob@havoc.io>
2018-06-22 17:38:46 +0300

    Exported agent package bundle copying for testing.
    We'll need this bundle copying for end-to-end tests as well, so we need
    to export it.

--- pkg/agent/bundle.go
+++ pkg/agent/bundle.go
@@ -101,3 +101,49 @@ func executableForPlatform(goos, goarch string) (string, error) {
 	// Success.
 	return file.Name(), nil
 }
+
+// CopyBundleForTesting copies the agent bundle from it's build path alongside
+// the current executable. It is useful for copying the agent bundle next to the
+// current test executable.
+func CopyBundleForTesting() error {
+	// Compute the path to the test executable and its parent directory.
+	executablePath, err := os.Executable()
+	if err != nil {
+		return errors.Wrap(err, "unable to compute test executable path")
+	}
+	testDirectory := filepath.Dir(executablePath)
+
+	// Compute the path to the agent bundle in the $GOPATH/bin directory.
+	var agentBundlePath string
+	if gopath := os.Getenv("GOPATH"); gopath == "" {
+		return errors.New("GOPATH not set")
+	} else {
+		agentBundlePath = filepath.Join(gopath, "bin", agentBundleName)
+	}
+
+	// Create a file that will be a copy of the agent bundle.
+	// HACK: We're assuming that Go runs test executables inside temporary
+	// directories that it cleans up, which does seem to be the case, but it'd
+	// be nice if there were some way to remove the agent bundle ourselves,
+	// maybe with some sort of atexit-like function.
+	bundleCopyFile, err := os.Create(filepath.Join(testDirectory, agentBundleName))
+	if err != nil {
+		return errors.Wrap(err, "unable to create agent bundle copy file")
+	}
+	defer bundleCopyFile.Close()
+
+	// Open the agent bundle.
+	bundleFile, err := os.Open(agentBundlePath)
+	if err != nil {
+		return errors.Wrap(err, "unable to open agent bundle file")
+	}
+	defer bundleFile.Close()
+
+	// Copy agent bundle contents.
+	if _, err := io.Copy(bundleCopyFile, bundleFile); err != nil {
+		return errors.Wrap(err, "unable to copy bundle file contents")
+	}
+
+	// Success.
+	return nil
+}
--- pkg/agent/bundle_test.go
+++ pkg/agent/bundle_test.go
@@ -1,9 +1,7 @@
 package agent
 
 import (
-	"io"
 	"os"
-	"path/filepath"
 	"runtime"
 	"testing"
 
@@ -11,42 +9,11 @@ import (
 )
 
 func init() {
-	// Compute the path to the test executable and its parent directory.
-	executablePath, err := os.Executable()
-	if err != nil {
-		panic(errors.Wrap(err, "unable to compute test executable path"))
-	}
-	testDirectory := filepath.Dir(executablePath)
-
-	// Compute the path to the agent bundle in the $GOPATH/bin directory.
-	var agentBundlePath string
-	if gopath := os.Getenv("GOPATH"); gopath == "" {
-		panic(errors.New("GOPATH not set"))
-	} else {
-		agentBundlePath = filepath.Join(gopath, "bin", agentBundleName)
-	}
-
-	// Create a file that will be a copy of the agent bundle.
-	// HACK: We're assuming that Go runs test executables inside temporary
-	// directories that it cleans up, which does seem to be the case, but it'd
-	// be nice if there were some way to remove the agent bundle ourselves,
-	// maybe with some sort of atexit-like function.
-	bundleCopyFile, err := os.Create(filepath.Join(testDirectory, agentBundleName))
-	if err != nil {
-		panic(errors.Wrap(err, "unable to create agent bundle copy file"))
-	}
-	defer bundleCopyFile.Close()
-
-	// Open the agent bundle.
-	bundleFile, err := os.Open(agentBundlePath)
-	if err != nil {
-		panic(errors.Wrap(err, "unable to open agent bundle file"))
-	}
-	defer bundleFile.Close()
-
-	// Copy agent bundle contents.
-	if _, err := io.Copy(bundleCopyFile, bundleFile); err != nil {
-		panic(errors.Wrap(err, "unable to copy bundle file contents"))
+	// Copy the agent bundle for testing.
+	// HACK: We're relying on the fact that Go will clean this up when it
+	// removes the testing temporary directory.
+	if err := CopyBundleForTesting(); err != nil {
+		panic(errors.Wrap(err, "unable to copy agent bundle for testing"))
 	}
 }
 
