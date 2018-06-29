4b5a79e4c6177fab1c145702b5b941af00d92b8f
Jacob Howard <jacob@havoc.io>
2018-06-29 18:46:05 +0300

    Switched to development of v0.4.x.
    We're going to start pulling in some not-quite-patch level changes to
    filesystem watching, and I think we should increment the minor version
    number.

--- pkg/mutagen/version.go
+++ pkg/mutagen/version.go
@@ -12,9 +12,9 @@ const (
 	// VersionMajor represents the current major version of Mutagen.
 	VersionMajor = 0
 	// VersionMinor represents the current minor version of Mutagen.
-	VersionMinor = 3
+	VersionMinor = 4
 	// VersionPatch represents the current patch version of Mutagen.
-	VersionPatch = 1
+	VersionPatch = 0
 )
 
 var Version string
