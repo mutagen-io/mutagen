ab802934a10634b4b8b0063e982a0782763874fa
Jacob Howard <jacob@havoc.io>
2018-07-01 01:02:43 +0300

    Revert "Made short listings a little bit more useful."
    This reverts commit b2240f45aa7aa1079806a79cd1bcded3e4f35a0c.

--- cmd/mutagen/list_monitor_common.go
+++ cmd/mutagen/list_monitor_common.go
@@ -12,63 +12,58 @@ func printSession(state *sessionpkg.State, long bool) {
 	// Print the session identifier.
 	fmt.Println("Session:", state.Session.Identifier)
 
-	// Extract configuration.
-	configuration := state.Session.Configuration
+	// Print extended information, if desired.
+	if long {
+		// Extract configuration.
+		configuration := state.Session.Configuration
 
-	// Compute and print the VCS ignore mode.
-	ignoreVCSModeDescription := configuration.IgnoreVCSMode.Description()
-	if configuration.IgnoreVCSMode == sync.IgnoreVCSMode_IgnoreVCSDefault {
-		defaultIgnoreVCSMode := state.Session.Version.DefaultIgnoreVCSMode()
-		ignoreVCSModeDescription += fmt.Sprintf(" (%s)", defaultIgnoreVCSMode.Description())
-	}
-	fmt.Println("Ignore VCS mode:", ignoreVCSModeDescription)
-
-	// Print default ignores.
-	if len(configuration.DefaultIgnores) == 0 {
-		fmt.Println("Default ignores: None")
-	} else if long {
-		fmt.Println("Default ignores:")
-		for _, p := range configuration.DefaultIgnores {
-			fmt.Printf("\t%s\n", p)
+		// Compute and print the VCS ignore mode.
+		ignoreVCSModeDescription := configuration.IgnoreVCSMode.Description()
+		if configuration.IgnoreVCSMode == sync.IgnoreVCSMode_IgnoreVCSDefault {
+			defaultIgnoreVCSMode := state.Session.Version.DefaultIgnoreVCSMode()
+			ignoreVCSModeDescription += fmt.Sprintf(" (%s)", defaultIgnoreVCSMode.Description())
 		}
-	} else {
-		fmt.Println("Default ignores:", len(configuration.DefaultIgnores))
-	}
+		fmt.Println("Ignore VCS mode:", ignoreVCSModeDescription)
 
-	// Print per-session ignores.
-	if len(configuration.Ignores) == 0 {
-		fmt.Println("Ignores: None")
-	} else if long {
-		fmt.Println("Ignores:")
-		for _, p := range configuration.Ignores {
-			fmt.Printf("\t%s\n", p)
+		// Print default ignores.
+		if len(configuration.DefaultIgnores) > 0 {
+			fmt.Println("Default ignores:")
+			for _, p := range configuration.DefaultIgnores {
+				fmt.Printf("\t%s\n", p)
+			}
 		}
-	} else {
-		fmt.Println("Ignores:", len(configuration.Ignores))
-	}
 
-	// Compute and print symlink mode.
-	symlinkModeDescription := configuration.SymlinkMode.Description()
-	if configuration.SymlinkMode == sync.SymlinkMode_SymlinkDefault {
-		defaultSymlinkMode := state.Session.Version.DefaultSymlinkMode()
-		symlinkModeDescription += fmt.Sprintf(" (%s)", defaultSymlinkMode.Description())
-	}
-	fmt.Println("Symlink mode:", symlinkModeDescription)
+		// Print per-session ignores.
+		if len(configuration.Ignores) > 0 {
+			fmt.Println("Ignores:")
+			for _, p := range configuration.Ignores {
+				fmt.Printf("\t%s\n", p)
+			}
+		}
 
-	// Compute and print the watch mode.
-	watchModeDescription := configuration.WatchMode.Description()
-	if configuration.WatchMode == filesystem.WatchMode_WatchDefault {
-		defaultWatchMode := state.Session.Version.DefaultWatchMode()
-		watchModeDescription += fmt.Sprintf(" (%s)", defaultWatchMode.Description())
-	}
-	fmt.Println("Watch mode:", watchModeDescription)
+		// Compute and print symlink mode.
+		symlinkModeDescription := configuration.SymlinkMode.Description()
+		if configuration.SymlinkMode == sync.SymlinkMode_SymlinkDefault {
+			defaultSymlinkMode := state.Session.Version.DefaultSymlinkMode()
+			symlinkModeDescription += fmt.Sprintf(" (%s)", defaultSymlinkMode.Description())
+		}
+		fmt.Println("Symlink mode:", symlinkModeDescription)
 
-	// Compute and print the polling interval.
-	var watchPollingIntervalDescription string
-	if configuration.WatchPollingInterval == 0 {
-		watchPollingIntervalDescription = fmt.Sprintf("Default (%d seconds)", filesystem.DefaultPollingInterval)
-	} else {
-		watchPollingIntervalDescription = fmt.Sprintf("%d seconds", configuration.WatchPollingInterval)
+		// Compute and print the watch mode.
+		watchModeDescription := configuration.WatchMode.Description()
+		if configuration.WatchMode == filesystem.WatchMode_WatchDefault {
+			defaultWatchMode := state.Session.Version.DefaultWatchMode()
+			watchModeDescription += fmt.Sprintf(" (%s)", defaultWatchMode.Description())
+		}
+		fmt.Println("Watch mode:", watchModeDescription)
+
+		// Compute and print the polling interval.
+		var watchPollingIntervalDescription string
+		if configuration.WatchPollingInterval == 0 {
+			watchPollingIntervalDescription = fmt.Sprintf("Default (%d seconds)", filesystem.DefaultPollingInterval)
+		} else {
+			watchPollingIntervalDescription = fmt.Sprintf("%d seconds", configuration.WatchPollingInterval)
+		}
+		fmt.Println("Watch polling interval:", watchPollingIntervalDescription)
 	}
-	fmt.Println("Watch polling interval:", watchPollingIntervalDescription)
 }
