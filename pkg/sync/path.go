3f638d1bdfe33d2848ba2ed1a741629da14af452
Jacob Howard <jacob@havoc.io>
2018-07-13 11:13:48 +0300

    Fixed our custom path joining function.
    We forgot to handle the case where the base is empty (i.e. the case
    where we're joining to the root path).

--- pkg/sync/path.go
+++ pkg/sync/path.go
@@ -5,5 +5,11 @@ package sync
 // in-memory paths, not filesystem paths, for which path/filepath.Join should
 // still be used.
 func pathJoin(base, leaf string) string {
+	// When joining a path to the root, we don't want to concatenate.
+	if base == "" {
+		return leaf
+	}
+
+	// Concatenate the paths.
 	return base + "/" + leaf
 }
