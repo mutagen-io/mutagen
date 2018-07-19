c47609d802f6319ae67f7b501b5606b826e51627
Jacob Howard <jacob@havoc.io>
2018-07-19 16:09:50 +0300

    Fixed watchRootParametersEqual for nil os.FileInfo on Windows.
    This commit fixes watchRootParametersEqual for nil os.FileInfo on
    Windows. It adds a fix for POSIX as well, which technically isn't
    necessary given the current os package implementation, but I don't want
    to have to rely on that implementation.

--- pkg/filesystem/watch_native_posix.go
+++ pkg/filesystem/watch_native_posix.go
@@ -1,11 +1,24 @@
-// +build !windows
+// +build !windows,!plan9
 
 package filesystem
 
 import (
 	"os"
+	"syscall"
 )
 
 func watchRootParametersEqual(first, second os.FileInfo) bool {
-	return os.SameFile(first, second)
+	// Watch out for nil file information.
+	if first == nil || second == nil {
+		return false
+	}
+
+	// Extract the underlying metadata.
+	firstData, firstOk := first.Sys().(*syscall.Stat_t)
+	secondData, secondOk := second.Sys().(*syscall.Stat_t)
+
+	// Check for equality.
+	return firstOk && secondOk &&
+		firstData.Dev == secondData.Dev &&
+		firstData.Ino == secondData.Ino
 }
--- pkg/filesystem/watch_native_windows.go
+++ pkg/filesystem/watch_native_windows.go
@@ -6,8 +6,16 @@ import (
 )
 
 func watchRootParametersEqual(first, second os.FileInfo) bool {
+	// Watch out for nil file information.
+	if first == nil || second == nil {
+		return false
+	}
+
+	// Extract the underlying metadata.
 	firstData, firstOk := first.Sys().(*syscall.Win32FileAttributeData)
 	secondData, secondOk := second.Sys().(*syscall.Win32FileAttributeData)
+
+	// Check for equality.
 	return firstOk && secondOk &&
 		firstData.FileAttributes == secondData.FileAttributes &&
 		firstData.CreationTime == secondData.CreationTime
