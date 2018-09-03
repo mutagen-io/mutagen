b9e474826071ffb41177cf832784291afe14db89
Jacob Howard <jacob@havoc.io>
2018-09-03 18:08:29 +0300

    Updated CI scripts for Go 1.11 and module migration.
    This commit updates our CI system scripts to behave with Go 1.11 and our
    modules-based build.

--- scripts/convert_windows_release_bundles.sh
+++ scripts/convert_windows_release_bundles.sh
@@ -4,14 +4,14 @@
 set -e
 
 # Compute the version.
-MUTAGEN_VERSION=$(mutagen version)
+MUTAGEN_VERSION=$(build/mutagen version)
 
 # Convert the 386 bundle.
-tar xzf build/mutagen_windows_386_v${MUTAGEN_VERSION}.tar.gz
-zip build/mutagen_windows_386_v${MUTAGEN_VERSION}.zip mutagen.exe mutagen-agents.tar.gz
+tar xzf build/release/mutagen_windows_386_v${MUTAGEN_VERSION}.tar.gz
+zip build/release/mutagen_windows_386_v${MUTAGEN_VERSION}.zip mutagen.exe mutagen-agents.tar.gz
 rm mutagen.exe mutagen-agents.tar.gz
 
 # Convert the amd64 bundle.
-tar xzf build/mutagen_windows_amd64_v${MUTAGEN_VERSION}.tar.gz
-zip build/mutagen_windows_amd64_v${MUTAGEN_VERSION}.zip mutagen.exe mutagen-agents.tar.gz
+tar xzf build/release/mutagen_windows_amd64_v${MUTAGEN_VERSION}.tar.gz
+zip build/release/mutagen_windows_amd64_v${MUTAGEN_VERSION}.zip mutagen.exe mutagen-agents.tar.gz
 rm mutagen.exe mutagen-agents.tar.gz
