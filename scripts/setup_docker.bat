a9d3240119156c91719265c8f1d79659d3d2c037
Jacob Howard <jacob@havoc.io>
2018-09-18 15:48:02 +0300

    Implemented Docker integration tests for AppVeyor.
    This commit adds Docker integration tests for AppVeyor. Something is
    still broken on the Travis CI Docker side.

---
+++ scripts/setup_docker.bat
@@ -0,0 +1,3 @@
+docker pull %MUTAGEN_TEST_DOCKER_BASE_IMAGE_NAME%
+docker build --tag %MUTAGEN_TEST_DOCKER_IMAGE_NAME% --file scripts/dockerfile_windows scripts
+docker run --name %MUTAGEN_TEST_DOCKER_CONTAINER_NAME% --detach %MUTAGEN_TEST_DOCKER_IMAGE_NAME%
---
+++ scripts/teardown_docker.bat
@@ -0,0 +1,4 @@
+docker stop %MUTAGEN_TEST_DOCKER_CONTAINER_NAME%
+docker container prune --force
+docker image rm --force %MUTAGEN_TEST_DOCKER_IMAGE_NAME%
+docker image rm --force %MUTAGEN_TEST_DOCKER_BASE_IMAGE_NAME%
