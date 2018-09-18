95c0d2b88379d7dcd1ebfa6aad4be1aa547cd28a
Jacob Howard <jacob@havoc.io>
2018-09-18 23:29:15 +0300

    Removed base image handling from AppVeyor Docker tests.
    It appears that AppVeyor has common Windows container images available
    already, so we can just use those directly.

--- scripts/setup_docker.bat
+++ scripts/setup_docker.bat
@@ -1,4 +1,3 @@
 docker version
-docker pull %MUTAGEN_TEST_DOCKER_BASE_IMAGE_NAME%
 docker build --tag %MUTAGEN_TEST_DOCKER_IMAGE_NAME% --file scripts/dockerfile_windows scripts
 docker run --name %MUTAGEN_TEST_DOCKER_CONTAINER_NAME% --detach %MUTAGEN_TEST_DOCKER_IMAGE_NAME%
--- scripts/teardown_docker.bat
+++ scripts/teardown_docker.bat
@@ -1,4 +1,3 @@
 docker stop %MUTAGEN_TEST_DOCKER_CONTAINER_NAME%
 docker container prune --force
 docker image rm --force %MUTAGEN_TEST_DOCKER_IMAGE_NAME%
-docker image rm --force %MUTAGEN_TEST_DOCKER_BASE_IMAGE_NAME%
