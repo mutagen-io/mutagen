d7c7183090eaf7a66df484fbd7deed5aad34fed9
Jacob Howard <jacob@havoc.io>
2018-12-12 13:54:07 +0200

    Added more extensive tests for Conflict.Root.
    This commit expands the test coverage for Conflict.Root. This is
    somewhat tricky logic that warrants full coverage.

--- pkg/sync/conflict_test.go
+++ pkg/sync/conflict_test.go
@@ -4,6 +4,279 @@ import (
 	"testing"
 )
 
+func TestConflictRootInvalid(t *testing.T) {
+	// Defer a handler that checks for a panic.
+	defer func() {
+		if recover() == nil {
+			t.Error("conflict root computation did not panic for invalid conflict")
+		}
+	}()
+
+	// Create an invalid conflict.
+	conflict := &Conflict{}
+
+	// Attempt to compute the root.
+	conflict.Root()
+}
+
+func TestConflictRootBothAtRoot(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "",
+				New:  testFile1Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "",
+				New:  testFile2Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := ""
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
+func TestConflictRootBothOneChangeAlphaAtRoot(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "",
+				Old:  testDirectory1Entry,
+				New:  testFile1Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "subpath",
+				New:  testFile1Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := ""
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
+func TestConflictRootBothOneChangeBetaAtRoot(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "subpath",
+				New:  testFile1Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "",
+				Old:  testDirectory1Entry,
+				New:  testFile1Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := ""
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
+func TestConflictRootBothOneChangeAlphaHigher(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "path",
+				Old:  testDirectory1Entry,
+				New:  testFile1Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "path/subpath",
+				New:  testFile1Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := "path"
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
+func TestConflictRootBothOneChangeBetaHigher(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "path/subpath",
+				New:  testFile1Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "path",
+				Old:  testDirectory1Entry,
+				New:  testFile1Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := "path"
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
+func TestConflictRootBetaMultipleAlphaAtRoot(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "",
+				Old:  testDirectory1Entry,
+				New:  testFile1Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "subpath",
+				New:  testFile1Entry,
+			},
+			{
+				Path: "subpath2",
+				New:  testFile2Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := ""
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
+func TestConflictRootAlphaMultipleBetaAtRoot(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "subpath",
+				New:  testFile1Entry,
+			},
+			{
+				Path: "subpath2",
+				New:  testFile2Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "",
+				Old:  testDirectory1Entry,
+				New:  testFile1Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := ""
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
+func TestConflictRootBetaMultipleAlphaHigher(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "path",
+				Old:  testDirectory1Entry,
+				New:  testFile1Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "path/subpath",
+				New:  testFile1Entry,
+			},
+			{
+				Path: "path/subpath2",
+				New:  testFile2Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := "path"
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
+func TestConflictRootAlphaMultipleBetaHigher(t *testing.T) {
+	// Create a test conflict.
+	conflict := &Conflict{
+		AlphaChanges: []*Change{
+			{
+				Path: "path/subpath",
+				New:  testFile1Entry,
+			},
+			{
+				Path: "path/subpath2",
+				New:  testFile2Entry,
+			},
+		},
+		BetaChanges: []*Change{
+			{
+				Path: "path",
+				Old:  testDirectory1Entry,
+				New:  testFile1Entry,
+			},
+		},
+	}
+
+	// Set the expected root.
+	expectedRoot := "path"
+
+	// Verify that the root is correct.
+	if conflict.Root() != expectedRoot {
+		t.Error("conflict root does not match expected:", conflict.Root(), "!=", expectedRoot)
+	}
+}
+
 func TestConflictNilInvalid(t *testing.T) {
 	var conflict *Conflict
 	if conflict.EnsureValid() == nil {
