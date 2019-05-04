fd9f389d4fa7af31c663af5c9f134d58ff19f021
Jacob Howard <jacob@havoc.io>
2019-05-05 02:33:18 +0300

    Refactored filesystem behavior probing fast paths for macOS.
    This commit restructures the filesystem behavior probing fast paths for
    macOS into a structure that's more amenable to extension and alignment
    with future implementations on other platforms.

--- pkg/filesystem/executability_posix_darwin.go
+++ pkg/filesystem/executability_posix_darwin.go
@@ -1,53 +1,42 @@
 package filesystem
 
-import (
-	"golang.org/x/sys/unix"
-)
-
-// extractExecutabilityPreservationFromStatfs trys to extract information about
-// executability preservation behavior from filesystem metadata by matching the
-// filesystem type to one with known behavior.
-func extractExecutabilityPreservationFromStatfs(filesystemMetadata *unix.Statfs_t) (bool, bool) {
-	// Check if the filesystem is APFS. APFS does preserve executability.
-	if statfsRepresentsAPFS(filesystemMetadata) {
+// probeExecutabilityPreservationFastByFormat checks if the specified format
+// matches well-known executability preservation behavior.
+func probeExecutabilityPreservationFastByFormat(format volumeFormat) (bool, bool) {
+	switch format {
+	case volumeFormatAPFS:
 		return true, true
-	}
-
-	// Check if the filesystem is HFS (or some variant of HFS). HFS does
-	// preserve executability.
-	if statfsRepresentsHFS(filesystemMetadata) {
+	case volumeFormatHFS:
 		return true, true
+	default:
+		return false, false
 	}
-
-	// Otherwise this is an unknown filesystem, so we're best off performing
-	// full probe operations.
-	return false, false
 }
 
 // probeExecutabilityPreservationFastByPath attempts to perform a fast
 // executability preservation test by path, without probe files. The
 // successfulness of the test is indicated by the second return parameter.
 func probeExecutabilityPreservationFastByPath(path string) (bool, bool) {
-	// Perform a filesystem metadata query on the directory.
-	var filesystemMetadata unix.Statfs_t
-	if unix.Statfs(path, &filesystemMetadata) != nil {
+	// Query the filesystem format.
+	format, err := queryVolumeFormatByPath(path)
+	if err != nil {
 		return false, false
 	}
 
-	// Extract information from the metadata.
-	return extractExecutabilityPreservationFromStatfs(&filesystemMetadata)
+	// Probe based on format.
+	return probeExecutabilityPreservationFastByFormat(format)
 }
 
 // probeExecutabilityPreservationFast attempts to perform a fast executability
 // preservation test, without probe files. The successfulness of the test is
 // indicated by the second return parameter.
 func probeExecutabilityPreservationFast(directory *Directory) (bool, bool) {
-	// Perform a filesystem metadata query on the directory.
-	var filesystemMetadata unix.Statfs_t
-	if unix.Fstatfs(directory.descriptor, &filesystemMetadata) != nil {
+	// Query the filesystem format.
+	format, err := queryVolumeFormat(directory)
+	if err != nil {
 		return false, false
 	}
 
-	// Extract information from the metadata.
-	return extractExecutabilityPreservationFromStatfs(&filesystemMetadata)
+	// Probe based on format.
+	return probeExecutabilityPreservationFastByFormat(format)
 }
--- pkg/filesystem/unicode_posix_darwin.go
+++ pkg/filesystem/unicode_posix_darwin.go
@@ -1,53 +1,42 @@
 package filesystem
 
-import (
-	"golang.org/x/sys/unix"
-)
-
-// extractUnicodeDecompositionFromStatfs trys to extract information about
-// Unicode decomposition behavior from filesystem metadata by matching the
-// filesystem type to one with known behavior.
-func extractUnicodeDecompositionFromStatfs(filesystemMetadata *unix.Statfs_t) (bool, bool) {
-	// Check if the filesystem is APFS. APFS does not decompose Unicode.
-	if statfsRepresentsAPFS(filesystemMetadata) {
+// probeUnicodeDecompositionFastByFormat checks if the specified format matches
+// well-known Unicode decomposition behavior.
+func probeUnicodeDecompositionFastByFormat(format volumeFormat) (bool, bool) {
+	switch format {
+	case volumeFormatAPFS:
 		return false, true
-	}
-
-	// Check if the filesystem is HFS (or some variant of HFS). HFS does
-	// decompose Unicode.
-	if statfsRepresentsHFS(filesystemMetadata) {
+	case volumeFormatHFS:
 		return true, true
+	default:
+		return false, false
 	}
-
-	// Otherwise this is an unknown filesystem, so we're best off performing
-	// full probe operations.
-	return false, false
 }
 
 // probeUnicodeDecompositionFast attempts to perform a fast Unicode
 // decomposition test by path, without probe files. The successfulness of the
 // test is indicated by the second return parameter.
 func probeUnicodeDecompositionFastByPath(path string) (bool, bool) {
-	// Perform a filesystem metadata query on the directory.
-	var filesystemMetadata unix.Statfs_t
-	if unix.Statfs(path, &filesystemMetadata) != nil {
+	// Query the filesystem format.
+	format, err := queryVolumeFormatByPath(path)
+	if err != nil {
 		return false, false
 	}
 
-	// Extract information from the metadata.
-	return extractUnicodeDecompositionFromStatfs(&filesystemMetadata)
+	// Probe based on format.
+	return probeUnicodeDecompositionFastByFormat(format)
 }
 
 // probeUnicodeDecompositionFast attempts to perform a fast Unicode
 // decomposition test, without probe files. The successfulness of the test is
 // indicated by the second return parameter.
 func probeUnicodeDecompositionFast(directory *Directory) (bool, bool) {
-	// Perform a filesystem metadata query on the directory.
-	var filesystemMetadata unix.Statfs_t
-	if unix.Fstatfs(directory.descriptor, &filesystemMetadata) != nil {
+	// Query the filesystem format.
+	format, err := queryVolumeFormat(directory)
+	if err != nil {
 		return false, false
 	}
 
-	// Extract information from the metadata.
-	return extractUnicodeDecompositionFromStatfs(&filesystemMetadata)
+	// Probe based on format.
+	return probeUnicodeDecompositionFastByFormat(format)
 }
