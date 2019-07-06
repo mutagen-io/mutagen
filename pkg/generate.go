2d36390ac90d791130cc0fa0cf4947dfecb6e2a8
Jacob Howard <jacob@havoc.io>
2019-07-05 23:01:44 -0700

    Renamed remote endpoint files.
    The file names in the remote endpoint package were a bit repetitive, so
    this commit renames them to be a bit simpler.

--- pkg/generate.go
+++ pkg/generate.go
@@ -5,7 +5,7 @@
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. service/prompt/prompt.proto
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. service/session/session.proto
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. session/configuration.proto session/scan_mode.proto session/selection.proto session/session.proto session/stage_mode.proto session/state.proto session/version.proto session/watch_mode.proto
-//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. session/endpoint/remote/endpoint_protocol.proto
+//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. session/endpoint/remote/protocol.proto
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. sync/archive.proto sync/cache.proto sync/change.proto sync/conflict.proto sync/entry.proto sync/ignore_vcs_mode.proto sync/mode.proto sync/problem.proto sync/symlink_mode.proto
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. url/url.proto
 //go:generate rm ./protoc-gen-go
