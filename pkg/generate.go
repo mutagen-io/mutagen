10eb45cc4595799d5670f7c4be0d878d69ae6dd7
Jacob Howard <jacob@havoc.io>
2019-07-11 22:19:28 -0700

    Disabled gRPC support in protoc-gen-go when not necessary.
    We only need gRPC support for our service packages, so in order to avoid
    inadvertently pulling in a gRPC dependency into our other packages
    (where it would bloat agent binaries), we disable support for gRPC
    outside of the service packages. This doesn't affect any generated code,
    because the protoc-gen-go generator is smart enough to know not to
    include a gRPC dependency needlessly, but this should explicitly guard
    against any inclusion.

--- pkg/generate.go
+++ pkg/generate.go
@@ -1,14 +1,14 @@
 //go:generate go build github.com/golang/protobuf/protoc-gen-go
-//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. filesystem/behavior/probe_mode.proto
-//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. selection/selection.proto
+//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. filesystem/behavior/probe_mode.proto
+//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. selection/selection.proto
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. service/daemon/daemon.proto
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. service/prompt/prompt.proto
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. service/synchronization/synchronization.proto
-//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. synchronization/configuration.proto synchronization/scan_mode.proto synchronization/session.proto synchronization/stage_mode.proto synchronization/state.proto synchronization/version.proto synchronization/watch_mode.proto
-//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. synchronization/core/archive.proto synchronization/core/cache.proto synchronization/core/change.proto synchronization/core/conflict.proto synchronization/core/entry.proto synchronization/core/ignore_vcs_mode.proto synchronization/core/mode.proto synchronization/core/problem.proto synchronization/core/symlink_mode.proto
-//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. synchronization/endpoint/remote/protocol.proto
-//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. synchronization/rsync/engine.proto synchronization/rsync/receive.proto synchronization/rsync/transmission.proto
-//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative,plugins=grpc:. url/url.proto
+//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. synchronization/configuration.proto synchronization/scan_mode.proto synchronization/session.proto synchronization/stage_mode.proto synchronization/state.proto synchronization/version.proto synchronization/watch_mode.proto
+//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. synchronization/core/archive.proto synchronization/core/cache.proto synchronization/core/change.proto synchronization/core/conflict.proto synchronization/core/entry.proto synchronization/core/ignore_vcs_mode.proto synchronization/core/mode.proto synchronization/core/problem.proto synchronization/core/symlink_mode.proto
+//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. synchronization/endpoint/remote/protocol.proto
+//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. synchronization/rsync/engine.proto synchronization/rsync/receive.proto synchronization/rsync/transmission.proto
+//go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. url/url.proto
 //go:generate rm ./protoc-gen-go
 
 package pkg
