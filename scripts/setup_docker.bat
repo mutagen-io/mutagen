e45321078794a9383151f07c1c4764e38f9dfedf
Jacob Howard <jacob@havoc.io>
2019-07-14 19:01:02 -0700

    Took another stab at fixing Windows Docker integration tests.
    The Windows Docker setup still doesn't want to run, so this commit tries
    a few additional fixes such as changing synax and path separators.

--- scripts/setup_docker.bat
+++ scripts/setup_docker.bat
@@ -1,5 +1,5 @@
-go build -o scripts/docker_windows/httpdemo.exe github.com/havoc-io/mutagen/pkg/integration/fixtures/httpdemo
+go build -o scripts\docker_windows\httpdemo.exe github.com/havoc-io/mutagen/pkg/integration/fixtures/httpdemo
 docker version
-docker image build --tag %MUTAGEN_TEST_DOCKER_IMAGE_NAME% scripts/docker_windows
-del /q scripts/docker_windows/httpdemo.exe
+docker image build --tag %MUTAGEN_TEST_DOCKER_IMAGE_NAME% scripts\docker_windows
+del /q scripts\docker_windows\httpdemo.exe
 docker container run --name %MUTAGEN_TEST_DOCKER_CONTAINER_NAME% --detach %MUTAGEN_TEST_DOCKER_IMAGE_NAME%
