fbf36243f69d6d6bdad6821eb29fb5743140a802
Jacob Howard <jacob@havoc.io>
2019-07-23 01:27:29 -0700

    Added the ability to override the Mutagen data directory
    This commit adds support for an environment variable,
    MUTAGEN_DATA_DIRECTORY, that allows users and embedding applications to
    override the Mutagen data directory path. This environment variable
    doesn't (and can't) affect the storage location for agents and their
    data, but that's not necessary since agents can share remote data
    directories without worrying about collisions.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- pkg/filesystem/mutagen.go
+++ pkg/filesystem/mutagen.go
@@ -64,34 +64,56 @@ const (
 // Mutagen computes (and optionally creates) subdirectories inside the Mutagen
 // data directory.
 func Mutagen(create bool, pathComponents ...string) (string, error) {
-	// Compute the path to the user's home directory.
-	homeDirectory, err := os.UserHomeDir()
-	if err != nil {
-		return "", errors.Wrap(err, "unable to compute path to home directory")
-	}
-
-	// Compute the path to the Mutagen data directory.
-	var mutagenDataDirectoryPath string
-	if !mutagen.DevelopmentVersion {
-		mutagenDataDirectoryPath = filepath.Join(homeDirectory, MutagenDataDirectoryName)
+	// Check if a data directory path has been explicitly specified. If not,
+	// compute it using the standard procedure. Also track whether or not we
+	// need to mark the directory as hidden on creation.
+	mutagenDataDirectoryPath, ok := os.LookupEnv("MUTAGEN_DATA_DIRECTORY")
+	var hide bool
+	if ok {
+		// Validate the provided path.
+		if mutagenDataDirectoryPath == "" {
+			return "", errors.New("provided data directory path is empty")
+		} else if !filepath.IsAbs(mutagenDataDirectoryPath) {
+			return "", errors.New("provided data directory path is not absolute")
+		}
 	} else {
-		mutagenDataDirectoryPath = filepath.Join(homeDirectory, MutagenDataDirectoryDevelopmentName)
+		// Compute the path to the user's home directory.
+		homeDirectory, err := os.UserHomeDir()
+		if err != nil {
+			return "", errors.Wrap(err, "unable to compute path to home directory")
+		}
+
+		// Compute the path to the Mutagen data directory.
+		if !mutagen.DevelopmentVersion {
+			mutagenDataDirectoryPath = filepath.Join(homeDirectory, MutagenDataDirectoryName)
+		} else {
+			mutagenDataDirectoryPath = filepath.Join(homeDirectory, MutagenDataDirectoryDevelopmentName)
+		}
+
+		// Flag the directory for hiding.
+		hide = true
 	}
 
 	// Compute the target path.
 	result := filepath.Join(mutagenDataDirectoryPath, filepath.Join(pathComponents...))
 
-	// If requested, attempt to create the Mutagen directory and the specified
-	// subpath. Also ensure that the Mutagen data directory is hidden.
+	// Handle directory creation, if requested.
+	//
 	// TODO: Should we iterate through each component and ensure the user hasn't
 	// changed the directory permissions? MkdirAll won't reset them. But I
 	// suppose the user may have changed them for whatever reason (though I
 	// can't imagine any).
 	if create {
+		// Create the directory.
 		if err := os.MkdirAll(result, 0700); err != nil {
 			return "", errors.Wrap(err, "unable to create subpath")
-		} else if err := MarkHidden(mutagenDataDirectoryPath); err != nil {
-			return "", errors.Wrap(err, "unable to hide Mutagen data directory")
+		}
+
+		// Mark the directory as hidden, if necessary.
+		if hide {
+			if err := MarkHidden(mutagenDataDirectoryPath); err != nil {
+				return "", errors.Wrap(err, "unable to hide Mutagen data directory")
+			}
 		}
 	}
 
