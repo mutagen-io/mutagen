23c67320f7e760ad2df375d2f1483ebe52ce5bd2
Jacob Howard <jacob@havoc.io>
2019-10-27 20:36:36 -0700

    Updated EnsureNameValid to support tunnel names.
    This commit updates EnsureNameValid to ensure that its error messages
    make sense for tunnels in addition to sessions.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- pkg/selection/names.go
+++ pkg/selection/names.go
@@ -6,17 +6,17 @@ import (
 	"github.com/pkg/errors"
 )
 
-// EnsureNameValid ensures that a name is valid for use as a session name. Empty
-// names are treated as valid.
+// EnsureNameValid ensures that a name is valid for use as a session or tunnel
+// name. Empty names are treated as valid.
 func EnsureNameValid(name string) error {
 	// Loop over the string and ensure that its characters are allowed. At the
 	// moment, the restrictions we apply here mirror those for Go identifiers.
-	// We intentionally disallow dashes to avoid collisions with session UUID
-	// identifiers. We might allow dashes (and maybe underscores) at some point
-	// if there's a demand, at which point we'd have to ensure that the name
-	// didn't match a UUID format (which might get a little expensive). The
-	// current set of allowed characters also work as keys in YAML without
-	// quoting.
+	// We intentionally disallow dashes (to avoid collisions with UUID
+	// identifiers) and underscores (to avoid colliding with other identifier
+	// formats). We might allow dashes and underscores at some point if there's
+	// a demand, at which point we'd have to ensure that the name doesn't
+	// collide with one of these identifier formats. The current set of allowed
+	// characters also work as keys in YAML without quoting.
 	for i, r := range name {
 		if unicode.IsLetter(r) {
 			continue
@@ -28,10 +28,10 @@ func EnsureNameValid(name string) error {
 		return errors.Errorf("invalid name character at index %d: '%c'", i, r)
 	}
 
-	// Disallow "defaults" as a session name since it is used as a special key
-	// in YAML files.
+	// Disallow "defaults" as a name since it is used as a special key in YAML
+	// files.
 	if name == "defaults" {
-		return errors.New("\"defaults\" is disallowed as a session name")
+		return errors.New(`"defaults" is disallowed as a name`)
 	}
 
 	// Success.
