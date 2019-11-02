da0a198d78118e5a6e7c119a1454e9ec568c95d7
Jacob Howard <jacob@havoc.io>
2019-11-02 20:19:41 +0000

    Removed outdated tunneling tests.
    We don't use the Role enumeration in tunneling anymore, so its tests
    needed to be removed.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- pkg/tunneling/tunnel_test.go
+++ pkg/tunneling/tunnel_test.go
@@ -1,32 +1,3 @@
 package tunneling
 
-import (
-	"testing"
-)
-
-// TestSupportedRoles verifies that tunnel role support is as expected.
-func TestSupportedRoles(t *testing.T) {
-	// Set up test cases.
-	testCases := []struct {
-		role     Role
-		expected bool
-	}{
-		{Role_Host, true},
-		{Role_Client, true},
-		{Role_Client + 1, false},
-	}
-
-	// Process test cases.
-	for _, testCase := range testCases {
-		if supported := testCase.role.Supported(); supported != testCase.expected {
-			t.Errorf(
-				"tunnel role (%s) support does not match expected: %t != %t",
-				testCase.role,
-				supported,
-				testCase.expected,
-			)
-		}
-	}
-}
-
 // TODO: Implement tests.
