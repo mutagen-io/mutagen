9836d242acf0977e91e4696709003020f6d085c1
Jacob Howard <jacob@havoc.io>
2019-11-30 07:13:16 -0800

    Allowed dashes in session and tunnel names.
    Dashes are useful delimiters in names, and we can still avoid collisions
    with UUIDs by checking formatting, so there's no real reason to disallow
    them.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- pkg/selection/names.go
+++ pkg/selection/names.go
@@ -4,19 +4,19 @@ import (
 	"unicode"
 
 	"github.com/pkg/errors"
+
+	"github.com/google/uuid"
 )
 
 // EnsureNameValid ensures that a name is valid for use as a session or tunnel
 // name. Empty names are treated as valid.
 func EnsureNameValid(name string) error {
-	// Loop over the string and ensure that its characters are allowed. At the
-	// moment, the restrictions we apply here mirror those for Go identifiers.
-	// We intentionally disallow dashes (to avoid collisions with UUID
-	// identifiers) and underscores (to avoid colliding with other identifier
-	// formats). We might allow dashes and underscores at some point if there's
-	// a demand, at which point we'd have to ensure that the name doesn't
-	// collide with one of these identifier formats. The current set of allowed
-	// characters also work as keys in YAML without quoting.
+	// Loop over the string and ensure that its characters are allowed. We allow
+	// letters, numbers, and dashses, but we require that the identifier starts
+	// with a letter. We disallow underscores to avoid colliding with internal
+	// identifiers. If a name contains dashes, then we enforce that it isn't a
+	// UUID to avoid collisions with legacy session identifiers.
+	var containsDash bool
 	for i, r := range name {
 		if unicode.IsLetter(r) {
 			continue
@@ -24,8 +24,18 @@ func EnsureNameValid(name string) error {
 			return errors.New("name does not start with Unicode letter")
 		} else if unicode.IsNumber(r) {
 			continue
+		} else if r == '-' {
+			containsDash = true
+			continue
 		}
 		return errors.Errorf("invalid name character at index %d: '%c'", i, r)
+	}
+
+	// If the name contains a dash, then ensure that it isn't a UUID.
+	if containsDash {
+		if _, err := uuid.Parse(name); err == nil {
+			return errors.New("name must not be a UUID")
+		}
 	}
 
 	// Disallow "defaults" as a name since it is used as a special key in YAML
