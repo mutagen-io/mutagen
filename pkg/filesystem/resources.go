72878e18123a81bf569bf0fbb6f9782413132a5f
Jacob Howard <jacob@havoc.io>
2019-12-01 11:53:23 -0800

    Added function to compute expected libexec path.
    This function is going to be necessary in order to implement more
    idiomatic Mutagen installations. It's also going to be necessary to
    implement tunnel agent searches.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

---
+++ pkg/filesystem/resources.go
@@ -0,0 +1,43 @@
+package filesystem
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// LibexecPath computes the expected libexec path assuming a typical POSIX
+// filesystem structure with the current executable located in the bin
+// directory. It does not verify that the directory exists.
+func LibexecPath() (string, error) {
+	// Compute the path to the current executable.
+	executablePath, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("unable to compute executable path: %w", err)
+	}
+
+	// If the executable path is a symbolic link, then perform resolution.
+	// Unfortunately there's no way to do this in a completely race-free
+	// fashion, but we're dealing with system prefixes here so it shouldn't be a
+	// problem.
+	if metadata, err := os.Lstat(executablePath); err != nil {
+		return "", fmt.Errorf("unable to read executable metadata: %w", err)
+	} else if metadata.Mode()&os.ModeSymlink != 0 {
+		if target, err := os.Readlink(executablePath); err != nil {
+			return "", fmt.Errorf("unable to read executable symbolic link target: %w", err)
+		} else if resolved, err := filepath.Abs(filepath.Join(filepath.Dir(executablePath), target)); err != nil {
+			return "", fmt.Errorf("unable to resolve executable symbolic link target: %w", err)
+		} else {
+			executablePath = resolved
+		}
+	}
+
+	// Check that the executable resides within a bin directory.
+	if filepath.Base(filepath.Dir(executablePath)) != "bin" {
+		return "", errors.New("executable does not reside within bin directory")
+	}
+
+	// Compute the expected libexec path.
+	return filepath.Clean(filepath.Join(executablePath, "..", "..", "libexec")), nil
+}
---
+++ pkg/filesystem/resources_test.go
@@ -0,0 +1,5 @@
+package filesystem
+
+// TODO: Implement tests. We'll need to figure out how to make LibexecPath work
+// in a test environment since it performs checks involving the running
+// executable's path.
