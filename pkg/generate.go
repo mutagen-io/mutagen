a57662fc85336de8cd7e99d2e11a6a6f60561ee3
Jacob Howard <jacob@havoc.io>
2019-12-02 13:56:42 -0800

    Added "generate" build tag to generate.go.
    This commit adds a "generate" build constraint to generate.go to ensure
    that it's never included in builds. This isn't necessary, but it is a
    useful safety guarantee.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- pkg/generate.go
+++ pkg/generate.go
@@ -1,3 +1,5 @@
+// +build generate
+
 //go:generate go build -mod=readonly github.com/golang/protobuf/protoc-gen-go
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. filesystem/behavior/probe_mode.proto
 //go:generate protoc --plugin=./protoc-gen-go -I. --go_out=paths=source_relative:. forwarding/configuration.proto forwarding/session.proto forwarding/socket_overwrite_mode.proto forwarding/state.proto forwarding/version.proto
