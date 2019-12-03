0c5ca3da50787012a6151431a328a1d2bdcbb230
Jacob Howard <jacob@havoc.io>
2019-12-02 18:50:17 -0800

    Revert "Removed "Experimental" label from forward and project commands."
    This reverts commit 9dcb16a3379d827fbbbb391e0d8705c01a34ae61.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- cmd/mutagen/forward/main.go
+++ cmd/mutagen/forward/main.go
@@ -2,6 +2,8 @@ package forward
 
 import (
 	"github.com/spf13/cobra"
+
+	"github.com/fatih/color"
 )
 
 func rootMain(command *cobra.Command, arguments []string) error {
@@ -29,6 +31,9 @@ var rootConfiguration struct {
 }
 
 func init() {
+	// Mark the command as experimental.
+	RootCommand.Short = RootCommand.Short + color.YellowString(" [Experimental]")
+
 	// Grab a handle for the command line flags.
 	flags := RootCommand.Flags()
 
