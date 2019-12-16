2746ab847a68c3855a9353ef8fd6794ea8b81b1b
Jacob Howard <jacob@havoc.io>
2019-12-16 08:54:47 -0800

    Bumped version to v0.11.0-alpha7.
    This commit bumps the version to v0.11.0-alpha7.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- pkg/mutagen/version.go
+++ pkg/mutagen/version.go
@@ -21,7 +21,7 @@ const (
 	// VersionTag represents a tag to be appended to the Mutagen version string.
 	// It must not contain spaces. If empty, no tag is appended to the version
 	// string.
-	VersionTag = "alpha6"
+	VersionTag = "alpha7"
 )
 
 // Version provides a stringified version of the current Mutagen version.
