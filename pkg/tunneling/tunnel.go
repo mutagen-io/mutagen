mutagen-io/mutagen

commit 9dd68a898b67c1ab385aa4d7932dfe502da02c9b

Refactored identifier creation to ensure consistent length.
This commit refactors the way that identifiers are created in order to
ensure a consistent total length of 48 characters without sacrificing
entropy. The core change is that the Base62-encoded portion of the
identifier is now left-padded with 0s to ensure it reaches the target
length. These 0s have no effect on the underlying encoded value.

Signed-off-by: Jacob Howard <jacob@havoc.io>

pkg/tunneling/tunnel.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/mutagen-io/mutagen/pkg/identifier"
 	"github.com/mutagen-io/mutagen/pkg/selection"
 )
 
@@ -15,8 +16,8 @@ func (p *TunnelHostCredentials) EnsureValid() error {
 	}
 
 	// Ensure that the tunnel identifier is valid.
-	if p.Identifier == "" {
-		return errors.New("empty tunnel identifier")
+	if !identifier.IsValid(p.Identifier) {
+		return errors.New("invalid tunnel identifier")
 	}
 
 	// Ensure that the tunnel version is supported.
@@ -56,8 +57,8 @@ func (t *Tunnel) EnsureValid() error {
 	}
 
 	// Ensure that the tunnel identifier is valid.
-	if t.Identifier == "" {
-		return errors.New("empty tunnel identifier")
+	if !identifier.IsValid(p.Identifier) {
+		return errors.New("invalid tunnel identifier")
 	}
 
 	// Ensure that the tunnel version is supported.

cmd/mutagen/project/flush.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/mutagen-io/mutagen/cmd/mutagen/sync"
 	"github.com/mutagen-io/mutagen/pkg/filesystem/locking"
+	"github.com/mutagen-io/mutagen/pkg/identifier"
 	"github.com/mutagen-io/mutagen/pkg/project"
 )
 
@@ -80,8 +81,9 @@ func flushMain(command *cobra.Command, arguments []string) error {
 		locker.Unlock()
 	}()
 
-	// Read the full contents of the lock file. If it's empty, then assume we
-	// created it and just remove it.
+	// Read the project identifier from the lock file. If the lock file is
+	// empty, then we can assume that we created it when we created the lock and
+	// just remove it.
 	buffer := &bytes.Buffer{}
 	if length, err := buffer.ReadFrom(locker); err != nil {
 		return errors.Wrap(err, "unable to read project lock")
@@ -89,10 +91,15 @@ func flushMain(command *cobra.Command, arguments []string) error {
 		removeLockFileOnReturn = true
 		return errors.New("project not running")
 	}
-	identifier := buffer.String()
+	projectIdentifier := buffer.String()
+
+	// Ensure that the project identifier is valid.
+	if !identifier.IsValid(projectIdentifier) {
+		return errors.New("invalid project identifier found in project lock")
+	}
 
 	// Compute the label selector that we're going to use to flush sessions.
-	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, identifier)
+	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, projectIdentifier)
 
 	// Flush synchronization sessions.
 	if err := sync.FlushWithLabelSelector(labelSelector, flushConfiguration.skipWait); err != nil {

cmd/mutagen/project/list.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mutagen-io/mutagen/cmd/mutagen/forward"
 	"github.com/mutagen-io/mutagen/cmd/mutagen/sync"
 	"github.com/mutagen-io/mutagen/pkg/filesystem/locking"
+	"github.com/mutagen-io/mutagen/pkg/identifier"
 	"github.com/mutagen-io/mutagen/pkg/project"
 )
 
@@ -81,8 +82,9 @@ func listMain(command *cobra.Command, arguments []string) error {
 		locker.Unlock()
 	}()
 
-	// Read the full contents of the lock file. If it's empty, then assume we
-	// created it and just remove it.
+	// Read the project identifier from the lock file. If the lock file is
+	// empty, then we can assume that we created it when we created the lock and
+	// just remove it.
 	buffer := &bytes.Buffer{}
 	if length, err := buffer.ReadFrom(locker); err != nil {
 		return errors.Wrap(err, "unable to read project lock")
@@ -90,10 +92,15 @@ func listMain(command *cobra.Command, arguments []string) error {
 		removeLockFileOnReturn = true
 		return errors.New("project not running")
 	}
-	identifier := buffer.String()
+	projectIdentifier := buffer.String()
+
+	// Ensure that the project identifier is valid.
+	if !identifier.IsValid(projectIdentifier) {
+		return errors.New("invalid project identifier found in project lock")
+	}
 
 	// Compute the label selector that we're going to use to list sessions.
-	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, identifier)
+	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, projectIdentifier)
 
 	// List forwarding sessions.
 	fmt.Println("Forwarding sessions:")

cmd/mutagen/project/pause.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mutagen-io/mutagen/cmd/mutagen/forward"
 	"github.com/mutagen-io/mutagen/cmd/mutagen/sync"
 	"github.com/mutagen-io/mutagen/pkg/filesystem/locking"
+	"github.com/mutagen-io/mutagen/pkg/identifier"
 	"github.com/mutagen-io/mutagen/pkg/project"
 )
 
@@ -81,8 +82,9 @@ func pauseMain(command *cobra.Command, arguments []string) error {
 		locker.Unlock()
 	}()
 
-	// Read the full contents of the lock file. If it's empty, then assume we
-	// created it and just remove it.
+	// Read the project identifier from the lock file. If the lock file is
+	// empty, then we can assume that we created it when we created the lock and
+	// just remove it.
 	buffer := &bytes.Buffer{}
 	if length, err := buffer.ReadFrom(locker); err != nil {
 		return errors.Wrap(err, "unable to read project lock")
@@ -90,10 +92,15 @@ func pauseMain(command *cobra.Command, arguments []string) error {
 		removeLockFileOnReturn = true
 		return errors.New("project not running")
 	}
-	identifier := buffer.String()
+	projectIdentifier := buffer.String()
+
+	// Ensure that the project identifier is valid.
+	if !identifier.IsValid(projectIdentifier) {
+		return errors.New("invalid project identifier found in project lock")
+	}
 
 	// Compute the label selector that we're going to use to pause sessions.
-	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, identifier)
+	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, projectIdentifier)
 
 	// Pause forwarding sessions.
 	if err := forward.PauseWithLabelSelector(labelSelector); err != nil {

cmd/mutagen/project/reset.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/mutagen-io/mutagen/cmd/mutagen/sync"
 	"github.com/mutagen-io/mutagen/pkg/filesystem/locking"
+	"github.com/mutagen-io/mutagen/pkg/identifier"
 	"github.com/mutagen-io/mutagen/pkg/project"
 )
 
@@ -80,8 +81,9 @@ func resetMain(command *cobra.Command, arguments []string) error {
 		locker.Unlock()
 	}()
 
-	// Read the full contents of the lock file. If it's empty, then assume we
-	// created it and just remove it.
+	// Read the project identifier from the lock file. If the lock file is
+	// empty, then we can assume that we created it when we created the lock and
+	// just remove it.
 	buffer := &bytes.Buffer{}
 	if length, err := buffer.ReadFrom(locker); err != nil {
 		return errors.Wrap(err, "unable to read project lock")
@@ -89,10 +91,15 @@ func resetMain(command *cobra.Command, arguments []string) error {
 		removeLockFileOnReturn = true
 		return errors.New("project not running")
 	}
-	identifier := buffer.String()
+	projectIdentifier := buffer.String()
+
+	// Ensure that the project identifier is valid.
+	if !identifier.IsValid(projectIdentifier) {
+		return errors.New("invalid project identifier found in project lock")
+	}
 
 	// Compute the label selector that we're going to use to reset sessions.
-	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, identifier)
+	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, projectIdentifier)
 
 	// Reset synchronization sessions.
 	if err := sync.ResetWithLabelSelector(labelSelector); err != nil {

cmd/mutagen/project/resume.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mutagen-io/mutagen/cmd/mutagen/forward"
 	"github.com/mutagen-io/mutagen/cmd/mutagen/sync"
 	"github.com/mutagen-io/mutagen/pkg/filesystem/locking"
+	"github.com/mutagen-io/mutagen/pkg/identifier"
 	"github.com/mutagen-io/mutagen/pkg/project"
 )
 
@@ -81,8 +82,9 @@ func resumeMain(command *cobra.Command, arguments []string) error {
 		locker.Unlock()
 	}()
 
-	// Read the full contents of the lock file. If it's empty, then assume we
-	// created it and just remove it.
+	// Read the project identifier from the lock file. If the lock file is
+	// empty, then we can assume that we created it when we created the lock and
+	// just remove it.
 	buffer := &bytes.Buffer{}
 	if length, err := buffer.ReadFrom(locker); err != nil {
 		return errors.Wrap(err, "unable to read project lock")
@@ -90,10 +92,15 @@ func resumeMain(command *cobra.Command, arguments []string) error {
 		removeLockFileOnReturn = true
 		return errors.New("project not running")
 	}
-	identifier := buffer.String()
+	projectIdentifier := buffer.String()
+
+	// Ensure that the project identifier is valid.
+	if !identifier.IsValid(projectIdentifier) {
+		return errors.New("invalid project identifier found in project lock")
+	}
 
 	// Compute the label selector that we're going to use to resume sessions.
-	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, identifier)
+	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, projectIdentifier)
 
 	// Resume forwarding sessions.
 	if err := forward.ResumeWithLabelSelector(labelSelector); err != nil {

cmd/mutagen/project/terminate.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mutagen-io/mutagen/cmd/mutagen/forward"
 	"github.com/mutagen-io/mutagen/cmd/mutagen/sync"
 	"github.com/mutagen-io/mutagen/pkg/filesystem/locking"
+	"github.com/mutagen-io/mutagen/pkg/identifier"
 	"github.com/mutagen-io/mutagen/pkg/project"
 )
 
@@ -81,8 +82,9 @@ func terminateMain(command *cobra.Command, arguments []string) error {
 		locker.Unlock()
 	}()
 
-	// Read the full contents of the lock file. If it's empty, then assume we
-	// created it and just remove it.
+	// Read the project identifier from the lock file. If the lock file is
+	// empty, then we can assume that we created it when we created the lock and
+	// just remove it.
 	buffer := &bytes.Buffer{}
 	if length, err := buffer.ReadFrom(locker); err != nil {
 		return errors.Wrap(err, "unable to read project lock")
@@ -90,10 +92,15 @@ func terminateMain(command *cobra.Command, arguments []string) error {
 		removeLockFileOnReturn = true
 		return errors.New("project not running")
 	}
-	identifier := buffer.String()
+	projectIdentifier := buffer.String()
+
+	// Ensure that the project identifier is valid.
+	if !identifier.IsValid(projectIdentifier) {
+		return errors.New("invalid project identifier found in project lock")
+	}
 
 	// Compute the label selector that we're going to use to terminate sessions.
-	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, identifier)
+	labelSelector := fmt.Sprintf("%s=%s", project.LabelKey, projectIdentifier)
 
 	// Terminate forwarding sessions.
 	if err := forward.TerminateWithLabelSelector(labelSelector); err != nil {

pkg/encoding/base62.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	// base62Alphabet is the alphabet used for Base62 encoding.
-	base62Alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	// Base62Alphabet is the alphabet used for Base62 encoding.
+	Base62Alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 )
 
 // base62 is the Base62 encoder. It is safe for concurrent use.
@@ -14,7 +14,7 @@ var base62 *basex.Encoding
 
 func init() {
 	// Initialize the Base62 encoder.
-	if encoding, err := basex.NewEncoding(base62Alphabet); err != nil {
+	if encoding, err := basex.NewEncoding(Base62Alphabet); err != nil {
 		panic("unable to initialize Base62 encoder")
 	} else {
 		base62 = encoding

commit d998e96e96d269f2144aef8efa30626efd0c934f

Fixed instance names in tunnel validation methods.
This commit fixes a typo introduced in 9dd68a8 that used an incorrect
instance variable name in Tunnel.EnsureValid. It also makes the instance
variable name more idiomatic in TunnelHostCredentials.EnsureValid.

Signed-off-by: Jacob Howard <jacob@havoc.io>

pkg/tunneling/tunnel.go
@@ -9,39 +9,39 @@ import (
 )
 
 // EnsureValid ensures that TunnelHostCredentials' invariants are respected.
-func (p *TunnelHostCredentials) EnsureValid() error {
+func (c *TunnelHostCredentials) EnsureValid() error {
 	// Ensure that the parameters are non-nil.
-	if p == nil {
+	if c == nil {
 		return errors.New("nil parameters")
 	}
 
 	// Ensure that the tunnel identifier is valid.
-	if !identifier.IsValid(p.Identifier) {
+	if !identifier.IsValid(c.Identifier) {
 		return errors.New("invalid tunnel identifier")
 	}
 
 	// Ensure that the tunnel version is supported.
-	if !p.Version.Supported() {
+	if !c.Version.Supported() {
 		return errors.New("unknown or unsupported tunnel version")
 	}
 
 	// Ensure that the creation time is present.
-	if p.CreationTime == nil {
+	if c.CreationTime == nil {
 		return errors.New("missing creation time")
 	}
 
 	// Ensure that the token is present.
-	if p.Token == "" {
+	if c.Token == "" {
 		return errors.New("empty token")
 	}
 
 	// Ensure that the secret has the correct length.
-	if len(p.Secret) != p.Version.secretLength() {
+	if len(c.Secret) != c.Version.secretLength() {
 		return errors.New("secret has incorrect length")
 	}
 
 	// Ensure that the configuration is valid.
-	if err := p.Configuration.EnsureValid(); err != nil {
+	if err := c.Configuration.EnsureValid(); err != nil {
 		return fmt.Errorf("invalid configuration: %w", err)
 	}
 
@@ -57,7 +57,7 @@ func (t *Tunnel) EnsureValid() error {
 	}
 
 	// Ensure that the tunnel identifier is valid.
-	if !identifier.IsValid(p.Identifier) {
+	if !identifier.IsValid(t.Identifier) {
 		return errors.New("invalid tunnel identifier")
 	}
 