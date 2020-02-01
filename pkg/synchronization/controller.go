mutagen-io/mutagen

commit 2da3c2420f7ecce2f67691d1a8ef9b1cab5cac1f

Updated polling to improve transport failure detection in no-watch mode.
This commit updates the synchronization loop polling mechanism to always
perform polling, even in the case of endpoints with a no-watch watch
mode. We still ignore events from these endpoints (which shouldn't be
sent in the well-behaved case anyway), but by polling on these
endpoints, we can better detect transport failures while the session is
sitting idle.

Signed-off-by: Jacob Howard <jacob@havoc.io>

pkg/synchronization/controller.go
@@ -841,14 +841,30 @@ func (c *controller) synchronize(context contextpkg.Context, alpha, beta Endpoin
 			pollContext, pollCancel := contextpkg.WithCancel(contextpkg.Background())
 
 			// Start alpha polling. If alpha has been put into a no-watch mode,
-			// then don't actually start polling there, because we don't want a
-			// malfunctioning (or malicious) endpoint to be able to force
-			// synchronization when it's not supposed to be able to.
+			// then we still perform polling in order to detect transport errors
+			// that might occur while the session is sitting idle, but we ignore
+			// any non-error responses and instead wait for the polling context
+			// to be cancelled. We perform this ignore operation because we
+			// don't want a broken or malicious endpoint to be able to force
+			// synchronization, especially if its watching has been
+			// intentionally disabled.
+			//
+			// It's worth noting that, because a well-behaved endpoint in
+			// no-watch mode never returns events, we'll always be polling on it
+			// (and thereby testing the transport) right up until the polling
+			// context is cancelled. Thus, there's no need to worry about cases
+			// where the endpoint sends back an event that we ignore and then
+			// has a transport failure without us noticing while we wait on the
+			// polling context (at least not for well-behaved endpoints).
 			αPollResults := make(chan error, 1)
 			go func() {
 				if αDisablePolling {
-					<-pollContext.Done()
-					αPollResults <- nil
+					if err := <-alpha.Poll(pollContext); err != nil {
+						αPollResults <- err
+					} else {
+						<-pollContext.Done()
+						αPollResults <- nil
+					}
 				} else {
 					αPollResults <- alpha.Poll(pollContext)
 				}
@@ -858,8 +874,12 @@ func (c *controller) synchronize(context contextpkg.Context, alpha, beta Endpoin
 			βPollResults := make(chan error, 1)
 			go func() {
 				if βDisablePolling {
-					<-pollContext.Done()
-					βPollResults <- nil
+					if err := <-beta.Poll(pollContext); err != nil {
+						βPollResults <- err
+					} else {
+						<-pollContext.Done()
+						βPollResults <- nil
+					}
 				} else {
 					βPollResults <- beta.Poll(pollContext)
 				}

commit 68125ccc56548f2f1fc606b0feae5018d93f3a95

Fixed syntax error with no-watch polling.
Commit 2da3c24 introduced a syntax error that we failed to catch, so
this fixes it.

Signed-off-by: Jacob Howard <jacob@havoc.io>

pkg/synchronization/controller.go
@@ -859,7 +859,7 @@ func (c *controller) synchronize(context contextpkg.Context, alpha, beta Endpoin
 			αPollResults := make(chan error, 1)
 			go func() {
 				if αDisablePolling {
-					if err := <-alpha.Poll(pollContext); err != nil {
+					if err := alpha.Poll(pollContext); err != nil {
 						αPollResults <- err
 					} else {
 						<-pollContext.Done()
@@ -874,7 +874,7 @@ func (c *controller) synchronize(context contextpkg.Context, alpha, beta Endpoin
 			βPollResults := make(chan error, 1)
 			go func() {
 				if βDisablePolling {
-					if err := <-beta.Poll(pollContext); err != nil {
+					if err := beta.Poll(pollContext); err != nil {
 						βPollResults <- err
 					} else {
 						<-pollContext.Done()