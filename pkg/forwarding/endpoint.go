5333a691bb5085b7114eec22e26a292e1cc7c023
Jacob Howard <jacob@havoc.io>
2020-01-31 17:33:30 -0800

    Fixed comment typo in forwarding endpoint.
    This commit fixes a typo (or at least some poor writing) in the comments
    for the forwarding endpoint definition.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- pkg/forwarding/endpoint.go
+++ pkg/forwarding/endpoint.go
@@ -8,10 +8,10 @@ import (
 // listening or dialing. In general, it is not safe for concurrent calls. It is,
 // however, required that Shutdown be callable concurrently with other methods.
 type Endpoint interface {
-	// Open should open a net connection for the endpoint. For listener (source)
-	// endpoints, this function should block until an incoming connection
-	// arrives. For dialer (destination) endpoints, this function should dial
-	// the underlying target.
+	// Open should open a network connection for the endpoint. For listener
+	// (source) endpoints, this function should block until an incoming
+	// connection arrives. For dialer (destination) endpoints, this function
+	// should dial the underlying target.
 	Open() (net.Conn, error)
 	// Shutdown shuts down the endpoint. This function should unblock any
 	// pending Open call.
