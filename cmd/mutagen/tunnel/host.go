mutagen-io/mutagen

commit 8d7208339a52d3d913f7583bd39c57815d3bf8c0

Added minor but important logging improvements.
This commit switches agents and tunnel hosts to use subloggers of the
root logger and adds critical additional logging to tunnel hosts.

Signed-off-by: Jacob Howard <jacob@havoc.io>

cmd/mutagen/tunnel/host.go
@@ -61,16 +61,19 @@ func hostMain(command *cobra.Command, arguments []string) error {
 		for {
 			severity, err := tunneling.HostTunnel(
 				context.Background(),
-				logging.RootLogger,
+				logging.RootLogger.Sublogger("hosting"),
 				hostCredentials,
 			)
 			switch severity {
 			case tunneling.ErrorSeverityRecoverable:
+				logger.Info("hosting restart due to recoverable error:", err)
 				continue
 			case tunneling.ErrorSeverityDelayedRecoverable:
+				logger.Info("delayed hosting restart due to recoverable error:", err)
 				time.Sleep(tunneling.HostTunnelRetryDelayTime)
 				continue
 			case tunneling.ErrorSeverityUnrecoverable:
+				logger.Info("hosting failed due to unrecoverable error:", err)
 				unrecoverableHostingErrors <- err
 				return
 			default:

cmd/mutagen-agent/forwarder.go
@@ -39,7 +39,10 @@ func forwarderMain(command *cobra.Command, arguments []string) error {
 	// termination.
 	forwardingTermination := make(chan error, 1)
 	go func() {
-		forwardingTermination <- remote.ServeEndpoint(logging.RootLogger, connection)
+		forwardingTermination <- remote.ServeEndpoint(
+			logging.RootLogger.Sublogger("forwarding"),
+			connection,
+		)
 	}()
 
 	// Wait for termination from a signal or the forwarder.

cmd/mutagen-agent/synchronizer.go
@@ -75,7 +75,10 @@ func synchronizerMain(command *cobra.Command, arguments []string) error {
 	// termination.
 	synchronizationTermination := make(chan error, 1)
 	go func() {
-		synchronizationTermination <- remote.ServeEndpoint(logging.RootLogger, connection)
+		synchronizationTermination <- remote.ServeEndpoint(
+			logging.RootLogger.Sublogger("synchronization"),
+			connection,
+		)
 	}()
 
 	// Wait for termination from a signal or the synchronizer.

commit 1a92d6a84a5267862cd050545817e16a1f66c2b9

Fixed refactoring typo.
This commit fixes a refactoring typo introduced in 8d72083.

Signed-off-by: Jacob Howard <jacob@havoc.io>

cmd/mutagen/tunnel/host.go
@@ -55,15 +55,14 @@ func hostMain(command *cobra.Command, arguments []string) error {
 		return fmt.Errorf("invalid host parameters: %w", err)
 	}
 
+	// Create a logger.
+	logger := logging.RootLogger.Sublogger("hosting")
+
 	// Perform hosting in a background Goroutine and track unrecoverable errors.
 	unrecoverableHostingErrors := make(chan error, 1)
 	go func() {
 		for {
-			severity, err := tunneling.HostTunnel(
-				context.Background(),
-				logging.RootLogger.Sublogger("hosting"),
-				hostCredentials,
-			)
+			severity, err := tunneling.HostTunnel(context.Background(), logger, hostCredentials)
 			switch severity {
 			case tunneling.ErrorSeverityRecoverable:
 				logger.Info("hosting restart due to recoverable error:", err)