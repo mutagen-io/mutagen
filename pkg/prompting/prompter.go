ef36db0f05e7914817b28328e56d054477760ea7
Jacob Howard <jacob@havoc.io>
2020-06-05 15:49:53 +0300

    Documented that prompters needn't be safe for concurrent usage.
    This commit clarifies the behavioral requirements of prompters.
    Specifically, it clarifies that they do not need to be safe for
    concurrent usage.
    
    Signed-off-by: Jacob Howard <jacob@havoc.io>

--- pkg/prompting/prompter.go
+++ pkg/prompting/prompter.go
@@ -1,6 +1,7 @@
 package prompting
 
 // Prompter is the interface to which types supporting prompting must adhere.
+// Implementations are not required to be safe for concurrent usage.
 type Prompter interface {
 	// Message should print a message to the user, returning an error if this is
 	// not possible.
