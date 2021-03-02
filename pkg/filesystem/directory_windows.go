mutagen-io/mutagen

commit e409a773d207e3ad8195868199669c5ed2bb17bd

Enforced Windows symbolic link detection consistency with Go.
We were previously using an incorrect (or at least not completely
correct) symbolic link detection mechanism that differed from that of
the Go standard library. On Windows, where we use the os package to
drive some of our filesystem implementation, this led to spurious
symbolic link reports. This commit unifies Mutagen's symbolic link
definition with that of the Go standard library.

Note that the Go standard library still has an open issue regarding
symbolic link classification with WSL symbolic links (golang/go#42184),
so we inherit that for now, but we should be able to adjust our
definition once that issue is resolved.

Fixes #248.

Signed-off-by: Jacob Howard <jacob@havoc.io>

pkg/filesystem/directory_windows.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"strings"
 	"syscall"
+	"unsafe"
 
 	"github.com/pkg/errors"
 
@@ -13,6 +14,8 @@ import (
 	aclapi "github.com/hectane/go-acl/api"
 
 	osvendor "github.com/mutagen-io/mutagen/pkg/filesystem/internal/third_party/os"
+
+	fssyscall "github.com/mutagen-io/mutagen/pkg/filesystem/internal/syscall"
 )
 
 // ensureValidName verifies that the provided name does not reference the
@@ -243,27 +246,26 @@ func (d *Directory) openHandle(name string, wantDirectory bool) (string, windows
 		return "", 0, errors.Wrap(err, "unable to open path")
 	}
 
-	// Query file metadata.
-	var metadata windows.ByHandleFileInformation
-	if err := windows.GetFileInformationByHandle(handle, &metadata); err != nil {
+	// Query attribute metadata.
+	var attributes fssyscall.FileAttributeTagInfo
+	if err := windows.GetFileInformationByHandleEx(
+		handle,
+		windows.FileAttributeTagInfo,
+		(*byte)(unsafe.Pointer(&attributes)),
+		uint32(unsafe.Sizeof(attributes)),
+	); err != nil {
 		windows.CloseHandle(handle)
-		return "", 0, errors.Wrap(err, "unable to query file metadata")
+		return "", 0, errors.Wrap(err, "unable to query attribute metadata")
 	}
 
-	// Verify that the handle does not represent a symbolic link and that the
-	// type coincides with what we want. Note that FILE_ATTRIBUTE_REPARSE_POINT
-	// can be or'd with FILE_ATTRIBUTE_DIRECTORY (since symbolic links are
-	// "typed" on Windows), so we have to explicitly exclude reparse points
-	// before checking types.
-	//
-	// TODO: Are there additional attributes upon which we should reject here?
-	// The Go os.File implementation doesn't seem to for normal os.Open
-	// operations, so I guess we don't need to either, but we should keep the
-	// option in mind.
-	if metadata.FileAttributes&windows.FILE_ATTRIBUTE_REPARSE_POINT != 0 {
+	// Verify that the attribute metadata doesn't indicate a symbolic link.
+	if attributes.IsSymbolicLink() {
 		windows.CloseHandle(handle)
 		return "", 0, errors.New("path pointed to symbolic link")
-	} else if wantDirectory && metadata.FileAttributes&windows.FILE_ATTRIBUTE_DIRECTORY == 0 {
+	}
+
+	// Verify that the handle corresponds to a directory (if requested).
+	if wantDirectory && attributes.FileAttributes&windows.FILE_ATTRIBUTE_DIRECTORY == 0 {
 		windows.CloseHandle(handle)
 		return "", 0, errors.New("path pointed to non-directory location")
 	}

pkg/filesystem/internal/syscall/syscall_windows.go
@@ -1,0 +1,27 @@
+package syscall
+
+import (
+	"golang.org/x/sys/windows"
+)
+
+// FileAttributeTagInfo is the Go representation of FILE_ATTRIBUTE_TAG_INFO.
+type FileAttributeTagInfo struct {
+	// FileAttributes are the file attributes.
+	FileAttributes uint32
+	// ReparseTag is the file reparse tag.
+	ReparseTag uint32
+}
+
+// IsSymbolicLink returns whether or not the file attributes indicate a symbolic
+// link. This method must match the implementation of os.fileStat.isSymlink in
+// the Go standard library in order for code in the filesystem package to work
+// correctly (since it partially relies on os package functionality on Windows
+// and will see inconsistent classification behavior otherwise).
+func (i *FileAttributeTagInfo) IsSymbolicLink() bool {
+	if i.FileAttributes&windows.FILE_ATTRIBUTE_REPARSE_POINT == 0 {
+		return false
+	}
+	// TODO: Update this definition once golang/go#42184 is resolved.
+	return i.ReparseTag == windows.IO_REPARSE_TAG_SYMLINK ||
+		i.ReparseTag == windows.IO_REPARSE_TAG_MOUNT_POINT
+}

pkg/filesystem/open_windows.go
@@ -4,12 +4,15 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"unsafe"
 
 	"github.com/pkg/errors"
 
 	"golang.org/x/sys/windows"
 
 	osvendor "github.com/mutagen-io/mutagen/pkg/filesystem/internal/third_party/os"
+
+	fssyscall "github.com/mutagen-io/mutagen/pkg/filesystem/internal/syscall"
 )
 
 // Open opens a filesystem path for traversal and operations. It will return
@@ -64,32 +67,26 @@ func Open(path string, allowSymbolicLinkLeaf bool) (io.Closer, *Metadata, error)
 		return nil, nil, errors.Wrap(err, "unable to open path")
 	}
 
-	// Query raw file metadata.
-	var rawMetadata windows.ByHandleFileInformation
-	if err := windows.GetFileInformationByHandle(handle, &rawMetadata); err != nil {
+	// Query attribute metadata.
+	var attributes fssyscall.FileAttributeTagInfo
+	if err := windows.GetFileInformationByHandleEx(
+		handle,
+		windows.FileAttributeTagInfo,
+		(*byte)(unsafe.Pointer(&attributes)),
+		uint32(unsafe.Sizeof(attributes)),
+	); err != nil {
 		windows.CloseHandle(handle)
-		return nil, nil, errors.Wrap(err, "unable to query file metadata")
+		return nil, nil, errors.Wrap(err, "unable to query attribute metadata")
 	}
 
-	// Verify that the handle does not represent a symbolic link. Even if we
-	// allow symbolic links in the leaf position of the path, we should not end
-	// up with one (it should resolve).
-	//
-	// Note that FILE_ATTRIBUTE_REPARSE_POINT can be or'd with
-	// FILE_ATTRIBUTE_DIRECTORY (since symbolic links are "typed" on Windows),
-	// so we have to explicitly exclude reparse points before checking types.
-	//
-	// TODO: Are there additional attributes upon which we should reject here?
-	// The Go os.File implementation doesn't seem to for normal os.Open
-	// operations, so I guess we don't need to either, but we should keep the
-	// option in mind.
-	if rawMetadata.FileAttributes&windows.FILE_ATTRIBUTE_REPARSE_POINT != 0 {
+	// Verify that the attribute metadata doesn't indicate a symbolic link.
+	if attributes.IsSymbolicLink() {
 		windows.CloseHandle(handle)
 		return nil, nil, ErrUnsupportedOpenType
 	}
 
 	// Determine whether or not this is a directory.
-	isDirectory := rawMetadata.FileAttributes&windows.FILE_ATTRIBUTE_DIRECTORY != 0
+	isDirectory := attributes.FileAttributes&windows.FILE_ATTRIBUTE_DIRECTORY != 0
 
 	// Handle os.File creation based on type.
 	var file *os.File

commit 319156a84d9bc9e18208fb9a2b4f71134aee3bac

Unified file handle metadata queries on Windows.
This commit unifies file handle metadata queries on Windows and fixes
support for non-NTFS filesystems (for which issues were introduced in
e409a77, similar to those in golang/go#29214).

Updates #248.

Signed-off-by: Jacob Howard <jacob@havoc.io>

pkg/filesystem/directory_windows.go
@@ -5,7 +5,6 @@ import (
 	"path/filepath"
 	"strings"
 	"syscall"
-	"unsafe"
 
 	"github.com/pkg/errors"
 
@@ -14,8 +13,6 @@ import (
 	aclapi "github.com/hectane/go-acl/api"
 
 	osvendor "github.com/mutagen-io/mutagen/pkg/filesystem/internal/third_party/os"
-
-	fssyscall "github.com/mutagen-io/mutagen/pkg/filesystem/internal/syscall"
 )
 
 // ensureValidName verifies that the provided name does not reference the
@@ -246,26 +243,21 @@ func (d *Directory) openHandle(name string, wantDirectory bool) (string, windows
 		return "", 0, errors.Wrap(err, "unable to open path")
 	}
 
-	// Query attribute metadata.
-	var attributes fssyscall.FileAttributeTagInfo
-	if err := windows.GetFileInformationByHandleEx(
-		handle,
-		windows.FileAttributeTagInfo,
-		(*byte)(unsafe.Pointer(&attributes)),
-		uint32(unsafe.Sizeof(attributes)),
-	); err != nil {
+	// Query file handle metadata.
+	isDirectory, isSymbolicLink, err := queryFileHandle(handle)
+	if err != nil {
 		windows.CloseHandle(handle)
-		return "", 0, errors.Wrap(err, "unable to query attribute metadata")
+		return "", 0, errors.Wrap(err, "unable to query file handle metadata")
 	}
 
-	// Verify that the attribute metadata doesn't indicate a symbolic link.
-	if attributes.IsSymbolicLink() {
+	// Verify that we're not dealing with a symbolic link.
+	if isSymbolicLink {
 		windows.CloseHandle(handle)
 		return "", 0, errors.New("path pointed to symbolic link")
 	}
 
 	// Verify that the handle corresponds to a directory (if requested).
-	if wantDirectory && attributes.FileAttributes&windows.FILE_ATTRIBUTE_DIRECTORY == 0 {
+	if wantDirectory && !isDirectory {
 		windows.CloseHandle(handle)
 		return "", 0, errors.New("path pointed to non-directory location")
 	}

pkg/filesystem/internal/syscall/syscall_windows.go
@@ -1,27 +1,9 @@
 package syscall
 
-import (
-	"golang.org/x/sys/windows"
-)
-
 // FileAttributeTagInfo is the Go representation of FILE_ATTRIBUTE_TAG_INFO.
 type FileAttributeTagInfo struct {
 	// FileAttributes are the file attributes.
 	FileAttributes uint32
 	// ReparseTag is the file reparse tag.
 	ReparseTag uint32
-}
-
-// IsSymbolicLink returns whether or not the file attributes indicate a symbolic
-// link. This method must match the implementation of os.fileStat.isSymlink in
-// the Go standard library in order for code in the filesystem package to work
-// correctly (since it partially relies on os package functionality on Windows
-// and will see inconsistent classification behavior otherwise).
-func (i *FileAttributeTagInfo) IsSymbolicLink() bool {
-	if i.FileAttributes&windows.FILE_ATTRIBUTE_REPARSE_POINT == 0 {
-		return false
-	}
-	// TODO: Update this definition once golang/go#42184 is resolved.
-	return i.ReparseTag == windows.IO_REPARSE_TAG_SYMLINK ||
-		i.ReparseTag == windows.IO_REPARSE_TAG_MOUNT_POINT
 }

pkg/filesystem/metadata_windows.go
@@ -1,0 +1,65 @@
+package filesystem
+
+import (
+	"fmt"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+
+	fssyscall "github.com/mutagen-io/mutagen/pkg/filesystem/internal/syscall"
+)
+
+// queryFileHandle performs a metadata query on a Windows file handle, returning
+// a pair of results indicating whether or not a file represents a directory and
+// whether or not a file represents a symbolic link (note that both can be true
+// in the case of directory symbolic links, because Windows symbolic links are
+// typed), as well as any error that occurs in making this determination. The
+// symbolic link classification behavior of this function must match that of the
+// os.fileStat.isSymlink method in the Go standard library in order for this
+// package to function correctly (because the Windows implementation of this
+// package partially relies on the standard os package and certain invariants
+// will break if this classification behavior differs).
+func queryFileHandle(handle windows.Handle) (bool, bool, error) {
+	// Perform a general file metadata query.
+	var metadata windows.ByHandleFileInformation
+	if err := windows.GetFileInformationByHandle(handle, &metadata); err != nil {
+		return false, false, fmt.Errorf("unable to query file metadata: %w", err)
+	}
+
+	// Determine whether or not we're dealing with a directory.
+	isDirectory := metadata.FileAttributes&windows.FILE_ATTRIBUTE_DIRECTORY != 0
+
+	// If the file attributes don't indicate a reparse point, then we're done.
+	if metadata.FileAttributes&windows.FILE_ATTRIBUTE_REPARSE_POINT == 0 {
+		return isDirectory, false, nil
+	}
+
+	// Since we're dealing with a reparse point, we need to perform an attribute
+	// query. Unfortunately this query isn't supported on some (perhaps all)
+	// non-NTFS filesystems, so we can't use it as our only query (even though
+	// it returns the same FileAttributes field as the general query above). In
+	// cases where this query returns an invalid parameter error, we assume that
+	// we're on a non-NTFS filesystem, in which case symbolic links aren't
+	// supported in any case. See golang/go#29214 for more information.
+	var attributes fssyscall.FileAttributeTagInfo
+	if err := windows.GetFileInformationByHandleEx(
+		handle,
+		windows.FileAttributeTagInfo,
+		(*byte)(unsafe.Pointer(&attributes)),
+		uint32(unsafe.Sizeof(attributes)),
+	); err != nil {
+		if err == windows.ERROR_INVALID_PARAMETER {
+			return isDirectory, false, nil
+		}
+		return false, false, fmt.Errorf("unable to query reparse point attributes: %w", err)
+	}
+
+	// Determine whether or not we're dealing with a symbolic link.
+	//
+	// TODO: Update this definition once golang/go#42184 is resolved.
+	isSymbolicLink := attributes.ReparseTag == windows.IO_REPARSE_TAG_SYMLINK ||
+		attributes.ReparseTag == windows.IO_REPARSE_TAG_MOUNT_POINT
+
+	// Success.
+	return isDirectory, isSymbolicLink, nil
+}

pkg/filesystem/open_windows.go
@@ -4,15 +4,12 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"unsafe"
 
 	"github.com/pkg/errors"
 
 	"golang.org/x/sys/windows"
 
 	osvendor "github.com/mutagen-io/mutagen/pkg/filesystem/internal/third_party/os"
-
-	fssyscall "github.com/mutagen-io/mutagen/pkg/filesystem/internal/syscall"
 )
 
 // Open opens a filesystem path for traversal and operations. It will return
@@ -47,9 +44,9 @@ func Open(path string, allowSymbolicLinkLeaf bool) (io.Closer, *Metadata, error)
 	// other threads or processes to delete or rename the file while open,
 	// avoids symbolic link traversal (at the path leaf), and has suitable
 	// semantics for both files and directories.
-	flags := uint32(windows.FILE_ATTRIBUTE_NORMAL | windows.FILE_FLAG_BACKUP_SEMANTICS | windows.FILE_FLAG_OPEN_REPARSE_POINT)
-	if allowSymbolicLinkLeaf {
-		flags = uint32(windows.FILE_ATTRIBUTE_NORMAL | windows.FILE_FLAG_BACKUP_SEMANTICS)
+	flags := uint32(windows.FILE_ATTRIBUTE_NORMAL | windows.FILE_FLAG_BACKUP_SEMANTICS)
+	if !allowSymbolicLinkLeaf {
+		flags |= windows.FILE_FLAG_OPEN_REPARSE_POINT
 	}
 	handle, err := windows.CreateFile(
 		path16,
@@ -67,26 +64,19 @@ func Open(path string, allowSymbolicLinkLeaf bool) (io.Closer, *Metadata, error)
 		return nil, nil, errors.Wrap(err, "unable to open path")
 	}
 
-	// Query attribute metadata.
-	var attributes fssyscall.FileAttributeTagInfo
-	if err := windows.GetFileInformationByHandleEx(
-		handle,
-		windows.FileAttributeTagInfo,
-		(*byte)(unsafe.Pointer(&attributes)),
-		uint32(unsafe.Sizeof(attributes)),
-	); err != nil {
+	// Query file handle metadata.
+	isDirectory, isSymbolicLink, err := queryFileHandle(handle)
+	if err != nil {
 		windows.CloseHandle(handle)
-		return nil, nil, errors.Wrap(err, "unable to query attribute metadata")
+		return nil, nil, errors.Wrap(err, "unable to query file handle metadata")
 	}
 
-	// Verify that the attribute metadata doesn't indicate a symbolic link.
-	if attributes.IsSymbolicLink() {
+	// Verify that we're not dealing with a symbolic link. If we are allowing
+	// symbolic links, then they should have been resolved by CreateFile.
+	if isSymbolicLink {
 		windows.CloseHandle(handle)
 		return nil, nil, ErrUnsupportedOpenType
 	}
-
-	// Determine whether or not this is a directory.
-	isDirectory := attributes.FileAttributes&windows.FILE_ATTRIBUTE_DIRECTORY != 0
 
 	// Handle os.File creation based on type.
 	var file *os.File
@@ -100,9 +90,13 @@ func Open(path string, allowSymbolicLinkLeaf bool) (io.Closer, *Metadata, error)
 		file = os.NewFile(uintptr(handle), path)
 	}
 
-	// Grab pre-converted metadata via the os.File object. For directories, this
-	// is path-based under the hood, but it's fine since we're already holding
-	// the directory handle open.
+	// Grab filesystem metadata via the os.File object. For directories, this is
+	// path-based under the hood, but it's fine since we're already holding the
+	// directory handle open. Unfortunately we can't easily fold this into our
+	// earlier metadata query on Windows because the mode conversion logic in
+	// the standard library (whose mode values we re-use on Windows) is quite
+	// complex. Fortunately this code is not on the hot path, so we don't incur
+	// too much penalty from the duplicate query operation.
 	fileMetadata, err := file.Stat()
 	if err != nil {
 		if isDirectory {