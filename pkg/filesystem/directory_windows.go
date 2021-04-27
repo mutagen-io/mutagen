3199de58a2e828b0af6a14392ceb72633286e63f
Jacob Howard <jacob@mutagen.io>
2021-04-27 22:39:37 +0300

    Fixed IsCrossDeviceError on Windows for direct MoveFileEx call.
    Now that we're calling MoveFileEx directly, we no longer unwrap a
    LinkError created by the os package.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- pkg/filesystem/directory_windows.go
+++ pkg/filesystem/directory_windows.go
@@ -545,21 +545,15 @@ func Rename(
 
 const (
 	// _ERROR_NOT_SAME_DEVICE is the error code returned by MoveFileEx on
-	// Windows when attempting to move a file across devices. This can actually
-	// be avoided on Windows by specifying the MOVEFILE_COPY_ALLOWED flag, but
-	// Go's standard library doesn't do this (most likely to keep consistency
-	// with POSIX, which has no such facility). Since we don't know the exact
-	// mechanism by which MOVEFILE_COPY_ALLOWED (i.e. whether or not it uses an
-	// intermediate file), we avoid using it via a direct call to MoveFileEx.
+	// Windows when attempting to move a file across devices (without the
+	// MOVEFILE_COPY_ALLOWED flag being specified).
 	_ERROR_NOT_SAME_DEVICE = 0x11
 )
 
 // IsCrossDeviceError checks whether or not an error returned from rename
 // represents a cross-device error.
 func IsCrossDeviceError(err error) bool {
-	if linkErr, ok := err.(*os.LinkError); !ok {
-		return false
-	} else if errno, ok := linkErr.Err.(syscall.Errno); !ok {
+	if errno, ok := err.(syscall.Errno); !ok {
 		return false
 	} else {
 		return errno == _ERROR_NOT_SAME_DEVICE
