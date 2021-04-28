9fcf53b1ed3c43bff10962d21312ceb4e361c11f
Jacob Howard <jacob@mutagen.io>
2021-04-28 15:13:43 +0300

    Removed setsid operation for transport processes on POSIX systems.
    We need setsid for the daemon, but not for transport processes, so this
    commit removes the setsid operation. This is faster and keeps the
    process hierarchy neater, so it's a no-brainer. It's also more
    consistent with the Windows model.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- pkg/agent/transport/process_posix.go
+++ pkg/agent/transport/process_posix.go
@@ -1,6 +1,4 @@
-// +build !windows,!plan9
-
-// TODO: Figure out what to do for Plan 9. It doesn't support Setsid.
+// +build !windows
 
 package transport
 
@@ -11,14 +9,5 @@ import (
 // ProcessAttributes returns the process attributes to use for starting
 // transport processes from the daemon.
 func ProcessAttributes() *syscall.SysProcAttr {
-	return &syscall.SysProcAttr{
-		// There's also a Noctty field, but it only detaches standard input from
-		// the controlling terminal (not standard output or error), and if
-		// standard input isn't a terminal, it will fail to launch the process.
-		// Setsid might be a little heavy handed since it creates a new process
-		// group, but it also properly detaches the process from any controlling
-		// terminal, and it's a standard system call, so it seems to be the most
-		// robust option.
-		Setsid: true,
-	}
+	return nil
 }
