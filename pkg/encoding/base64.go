6fb673f78bd749bdf7940f6a93d32d1792d0ee5c
Jacob Howard <jacob@mutagen.io>
2021-05-09 19:02:43 +0300

    Fixed comment typo.
    The word shorthand doesn't contain a hyphen.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- pkg/encoding/base64.go
+++ pkg/encoding/base64.go
@@ -4,12 +4,12 @@ import (
 	"encoding/base64"
 )
 
-// EncodeBase64 is short-hand for base64.RawURLEncoding.EncodeToString
+// EncodeBase64 is shorthand for base64.RawURLEncoding.EncodeToString
 func EncodeBase64(value []byte) string {
 	return base64.RawURLEncoding.EncodeToString(value)
 }
 
-// DecodeBase64 is short-hand for base64.RawURLEncoding.DecodeString.
+// DecodeBase64 is shorthand for base64.RawURLEncoding.DecodeString.
 func DecodeBase64(value string) ([]byte, error) {
 	return base64.RawURLEncoding.DecodeString(value)
 }
