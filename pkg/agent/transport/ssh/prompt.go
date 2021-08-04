13a4094c67869ebdd5ca4519ddc3bba4641765d8
Jacob Howard <jacob@mutagen.io>
2021-08-04 13:58:58 +0300

    Added support for SSH_ASKPASS_REQUIRE environment variable.
    OpenSSH 8.4 added a new SSH_ASKPASS_REQUIRE environment variable to
    provide more granular control over SSH_ASKPASS behavior, but it also
    changed the default prompting behavior, favoring the controlling
    terminal even if both SSH_ASKPASS and DISPLAY are set. This becomes a
    problem if the daemon is being run manually in the foreground.
    Fortunately this new mechanism is easy to adopt and provides more
    reliable control over SSH_ASKPASS behavior.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- pkg/agent/transport/ssh/prompt.go
+++ pkg/agent/transport/ssh/prompt.go
@@ -18,9 +18,9 @@ func SetPrompterVariables(environment []string, prompter string) ([]string, erro
 	// Handle based on whether or not there's a prompter.
 	if prompter == "" {
 		// If there is no prompter, then enforce that SSH_ASKPASS is not set,
-		// because some systems (e.g. systems with a Cygwin SSH binary) will
-		// include default SSH_ASKPASS values that throw up GUIs without any
-		// message or context and we don't want that.
+		// because some systems (e.g. systems with a Cygwin SSH binary) set
+		// default SSH_ASKPASS values that throw up GUIs without any message or
+		// context, which we don't want.
 		filteredEnvironment := environment[:0]
 		for _, e := range environment {
 			if !strings.HasPrefix(e, "SSH_ASKPASS=") {
@@ -29,7 +29,7 @@ func SetPrompterVariables(environment []string, prompter string) ([]string, erro
 		}
 		environment = filteredEnvironment
 	} else {
-		// Compute the path to the current (mutagen) executable and set it in
+		// Compute the path to the current (mutagen) executable and store it in
 		// the SSH_ASKPASS variable.
 		if mutagenPath, err := os.Executable(); err != nil {
 			return nil, errors.Wrap(err, "unable to determine executable path")
@@ -37,8 +37,19 @@ func SetPrompterVariables(environment []string, prompter string) ([]string, erro
 			environment = append(environment, fmt.Sprintf("SSH_ASKPASS=%s", mutagenPath))
 		}
 
-		// Set the DISPLAY variable to Mutagen.
-		environment = append(environment, "DISPLAY=mutagen")
+		// Ensure that the SSH_ASKPASS mechanism is going to be used, even if
+		// the process has a controlling terminal (e.g. when running the daemon
+		// manually in the foreground). Prior to OpenSSH 8.4, all that was
+		// needed was to set the DISPLAY variable to a non-empty value. In
+		// OpenSSH 8.4 and later, the controlling terminal (if one is set) will
+		// be used for prompting even if SSH_ASKPASS and DISPLAY are set, but an
+		// additional environment variable, SSH_ASKPASS_REQUIRE, has been added
+		// to provide more granular control of SSH_ASKPASS behavior, so we use
+		// that to force usage of the SSH_ASKPASS mechanism in this case.
+		environment = append(environment,
+			"DISPLAY=mutagen",
+			"SSH_ASKPASS_REQUIRE=force",
+		)
 
 		// Add the prompter environment variable to make Mutagen recognize a
 		// prompting invocation.
