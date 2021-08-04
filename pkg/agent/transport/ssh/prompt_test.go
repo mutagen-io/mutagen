45d1a13e605e59fb84056e867d60ef7e8f1574b3
Jacob Howard <jacob@mutagen.io>
2021-08-04 15:17:52 +0300

    Updated TestAddPrompterVariables for 13a4094.
    It seems we forgot to update this unit test as part of the original
    commit.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- pkg/agent/transport/ssh/prompt_test.go
+++ pkg/agent/transport/ssh/prompt_test.go
@@ -15,7 +15,7 @@ func TestAddPrompterVariablesNoPrompter(t *testing.T) {
 func TestAddPrompterVariables(t *testing.T) {
 	if e, err := SetPrompterVariables(nil, "prompter-id"); err != nil {
 		t.Fatal("failed to set prompter environment variables:", err)
-	} else if len(e) != 3 {
+	} else if len(e) != 4 {
 		t.Error("unexpected number of environment variables after adding prompter values")
 	}
 }
