599ba37ef824e041b19e32af445ca17bc6715309
Jacob Howard <jacob@mutagen.io>
2021-10-22 12:08:13 -0600

    Moved macOS notarization into a dedicated script.
    The build script is already a little crowded, so it makes sense to make
    this an isolated operation, especially since it's only performed on
    tagged releases.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

---
+++ scripts/ci/notarize.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Exit immediately on failure.
+set -e
+
+# Determine the operating system.
+MUTAGEN_OS_NAME="$(go env GOOS)"
+
+# Perform a build that's appropriate for the platform.
+if [[ "${MUTAGEN_OS_NAME}" == "darwin" ]]; then
+    # Generate the zip archives needed for uploading to the notarization server.
+    /usr/bin/ditto -c -k --keepParent build/cli/darwin_amd64 "${RUNNER_TEMP}/notarize_cli_darwin_amd64.zip"
+    /usr/bin/ditto -c -k --keepParent build/cli/darwin_arm64 "${RUNNER_TEMP}/notarize_cli_darwin_arm64.zip"
+    /usr/bin/ditto -c -k --keepParent build/agent/darwin_amd64 "${RUNNER_TEMP}/notarize_agent_darwin_amd64.zip"
+    /usr/bin/ditto -c -k --keepParent build/agent/darwin_arm64 "${RUNNER_TEMP}/notarize_agent_darwin_arm64.zip"
+
+    # Notarize each archive individually.
+    find "${RUNNER_TEMP}" -name 'notarize_*.zip' -exec \
+        xcrun notarytool submit \
+        --wait \
+        --apple-id "${MACOS_NOTARIZE_APPLE_ID}" \
+        --password "${MACOS_NOTARIZE_APP_SPECIFIC_PASSWORD}" \
+        --team-id "${MACOS_NOTARIZE_TEAM_ID}" \
+        {} \;
+
+    # Remove the archives.
+    rm "${RUNNER_TEMP}/notarize_*.zip"
+else
+    echo "This script is not supported on this platform"
+    exit 1
+fi
