5b82f83b23299330c3f8077cd34793e8a6285769
Jacob Howard <jacob@mutagen.io>
2021-10-26 17:25:23 -0600

    Bumped version to v0.13.0-beta1.
    This commit bumps the version to v0.13.0-beta1.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- pkg/mutagen/version.go
+++ pkg/mutagen/version.go
@@ -13,13 +13,13 @@ const (
 	// VersionMajor represents the current major version of Mutagen.
 	VersionMajor = 0
 	// VersionMinor represents the current minor version of Mutagen.
-	VersionMinor = 12
+	VersionMinor = 13
 	// VersionPatch represents the current patch version of Mutagen.
 	VersionPatch = 0
 	// VersionTag represents a tag to be appended to the Mutagen version string.
 	// It must not contain spaces. If empty, no tag is appended to the version
 	// string.
-	VersionTag = "beta8"
+	VersionTag = "beta1"
 )
 
 // Version provides a stringified version of the current Mutagen version.
