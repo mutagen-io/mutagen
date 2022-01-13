dff1429ff9ee3d25f1be3578b6c783e14231303d
Jacob Howard <jacob@mutagen.io>
2022-01-13 11:29:57 +0200

    Simplified sidecar tag script.
    For the time being, we'll avoid doing complicated multi-tag sidecar
    releases, because Mutagen Compose won't make use of them and other
    projects will likely want to maintain similar precision tags. As Mutagen
    matures and the sidecar matures, we might want to rethink this, but for
    the time being it makes sense to only support pinning to specific
    version tags.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- scripts/ci/sidecar_tags.go
+++ scripts/ci/sidecar_tags.go
@@ -2,32 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/mutagen-io/mutagen/pkg/mutagen"
 	"github.com/mutagen-io/mutagen/pkg/sidecar"
 )
 
 func main() {
-	// Track the tags that we need to push.
-	var tags []string
-
-	// Always create a tag for the full version.
-	tags = append(tags, fmt.Sprintf("%s:%s", sidecar.BaseTag, mutagen.Version))
-
-	// If this is a proper release version (i.e. it doesn't have a version tag),
-	// then add other tags as appropriate. In this case, we don't need to add
-	// the patch version, because that will be the full version added above.
-	if mutagen.VersionTag == "" {
-		// Add the minor version.
-		tags = append(tags, fmt.Sprintf("%s:%d.%d", sidecar.BaseTag, mutagen.VersionMajor, mutagen.VersionMinor))
-
-		// Add the major version if we're post-1.0.
-		if mutagen.VersionMajor >= 1 {
-			tags = append(tags, fmt.Sprintf("%s:%d", sidecar.BaseTag, mutagen.VersionMajor))
-		}
-	}
-
-	// Compute the combined tag list.
-	fmt.Println(strings.Join(tags, ","))
+	fmt.Printf("%s:%s\n", sidecar.BaseTag, mutagen.Version)
 }
