8795952bc94c93bb36f4213f93351cf24daff602
Jacob Howard <jacob@mutagen.io>
2022-01-13 13:32:52 +0200

    Removed Compose V1 support.
    Mutagen Compose (based on Compose V2) is now the official way forward
    for using Mutagen with Compose.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- pkg/compose/labels.go
+++
@@ -1,8 +0,0 @@
-package compose
-
-const (
-	// projectNameLabel is the label key for the project name.
-	projectNameLabel = "io.mutagen.compose.project.name"
-	// daemonIdentifierLabel is the label key for the daemon identifier.
-	daemonIdentifierLabel = "io.mutagen.compose.daemon.identifier"
-)
--- pkg/docker/daemon_test.go
+++
@@ -1,3 +0,0 @@
-package docker
-
-// TODO: Implement.
