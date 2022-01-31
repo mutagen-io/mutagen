564001958b6457cf3492746363bcfe54b9f3e4db
Jacob Howard <jacob@mutagen.io>
2022-01-31 15:40:36 +0200

    ci: add gofmt checking to analysis script
    The gofmt tool comes with an additional -s flag that performs code
    simplification. This isn't available via go fmt for some reason, but in
    any case we can perform this check (along with the rest of the checks
    that go fmt would make) from the gofmt tool.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- scripts/ci/analyze.sh
+++ scripts/ci/analyze.sh
@@ -4,15 +4,26 @@
 # fail, so we track the exit status of each command instead of using set -e.
 FAILURE=0
 
+# Verify that code is formatted and simplified according to Go standards. We
+# skip this check on Windows due to gofmt normalizing Go code to LF endings (and
+# thus generating an enormous and pointless diff).
+if [[ "$(go env GOOS)" != "windows" ]]; then
+    gofmt -s -l -d . | tee gofmt.log
+    if [[ -s gofmt.log ]]; then
+        FAILURE=1
+    fi
+    rm gofmt.log
+fi
+
 # Perform static analysis.
 go vet ./pkg/... || FAILURE=1
 go vet ./cmd/... || FAILURE=1
 go vet ./scripts/... || FAILURE=1
 
-# TODO: Add gofmt -s support.
-# TODO: Add golint (https://github.com/golang/lint).
-# TODO: Add ineffassign (https://github.com/gordonklaus/ineffassign).
-# TODO: Add misspell (https://github.com/client9/misspell).
+# TODO: Add spell checking. The https://github.com/client9/misspell tool is what
+# we've used historically (via Go Report Card), but it seems like it's no longer
+# maintained and it's installation is a little non-trivial.
+
 # TODO: Add custom code/comment structure validation.
 
 # Done.
