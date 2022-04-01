43b37a21c4e5382d91427c71c81128cca8dba841
Jacob Howard <jacob@mutagen.io>
2022-04-01 13:46:02 +0100

    cmd: disable flag sorting in all commands
    There were a few commands in the agent and sidecar for which flag
    sorting was not disabled. This wasn't causing any issues, because those
    commands only had a single -h/--help flag, but it's worth keeping this
    behavior and code consistent across all commands.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- cmd/mutagen-agent/forwarder.go
+++ cmd/mutagen-agent/forwarder.go
@@ -74,6 +74,9 @@ func init() {
 	// Grab a handle for the command line flags.
 	flags := forwarderCommand.Flags()
 
+	// Disable alphabetical sorting of flags in help output.
+	flags.SortFlags = false
+
 	// Manually add a help flag to override the default message. Cobra will
 	// still implement its logic automatically.
 	flags.BoolVarP(&forwarderConfiguration.help, "help", "h", false, "Show help information")
