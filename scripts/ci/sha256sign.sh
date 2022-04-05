0e7b50b1627a22c56a07986ff8376825e1b30333
Jacob Howard <jacob@mutagen.io>
2022-04-05 17:59:42 +0100

    ci: allow non-signed SHA256SUMS generation
    Previously, we completely disabled SHA256SUMS in cases where the GPG
    signing key wasn't available (usually pull requests from third-party
    repositories), but we can just split up the SHA256 computation step into
    two parts, that way we can still perform SHA256 digests for test builds,
    they just won't be signed.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- scripts/ci/sha256sign.sh
+++ scripts/ci/sha256sign.sh
@@ -6,8 +6,11 @@ set -e
 # Move to the release directory.
 pushd build/release > /dev/null
 
-# Compute SHA256 digests.
-sha256sum mutagen_* > SHA256SUMS
+# Sign the SHA256 digests file.
+gpg --detach-sign --armor \
+    --default-key "${SHA256_GPG_SIGNING_IDENTITY}" \
+    --output SHA256SUMS.gpg \
+    SHA256SUMS
 
 # Leave the release directory.
 popd > /dev/null
