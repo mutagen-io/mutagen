5ab9dec6d28126339889a123bcf6ca64755cfd24
Jacob Howard <jacob@mutagen.io>
2023-02-06 15:44:31 -0700

    all: enable SSPL enhancements by default in release builds
    Mutagen v0.17 is going to start shipping builds that include
    SSPL-licensed enhancements by default. We're not going to remove the
    ability to create builds with only MIT-licensed Mutagen code, and we'll
    continue to test that option, but we'll no longer ship those builds.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- scripts/ci/test_parameters.sh
+++ scripts/ci/test_parameters.sh
@@ -8,16 +8,19 @@ export MUTAGEN_TEST_END_TO_END="full"
 
 # Set platform-specific integration testing parameters.
 if [[ "${MUTAGEN_OS_NAME}" == "darwin" ]]; then
+    export MUTAGEN_TEST_ENABLE_SSPL="false"
     export MUTAGEN_TEST_SSH="true"
     export MUTAGEN_TEST_FAT32_ROOT="/Volumes/FAT32ROOT"
     export MUTAGEN_TEST_HFS_ROOT="/Volumes/HFSRoot"
     export MUTAGEN_TEST_APFS_ROOT="/Volumes/APFSRoot"
     export MUTAGEN_TEST_SUBFS_ROOT="${MUTAGEN_TEST_APFS_ROOT}/HFSSub"
 elif [[ "${MUTAGEN_OS_NAME}" == "linux" ]]; then
+    export MUTAGEN_TEST_ENABLE_SSPL="true"
     export MUTAGEN_TEST_SSH="true"
     export MUTAGEN_TEST_DOCKER="true"
     export MUTAGEN_TEST_DOCKER_CONTAINER_NAME="mutagentester"
 elif [[ "${MUTAGEN_OS_NAME}" == "windows" ]]; then
+    export MUTAGEN_TEST_ENABLE_SSPL="false"
     export MUTAGEN_TEST_DOCKER="true"
     export MUTAGEN_TEST_DOCKER_CONTAINER_NAME="mutagentester"
     export MUTAGEN_TEST_DOCKER_USERNAME="george"
