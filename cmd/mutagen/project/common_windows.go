750bc9032da3a8a4ca947fb2480a2774dce8379d
Jacob Howard <jacob@mutagen.io>
2023-04-13 15:37:17 -0600

    cli: improve validation during Windows shell resolution in projects
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- cmd/mutagen/project/common_windows.go
+++ cmd/mutagen/project/common_windows.go
@@ -1,17 +1,24 @@
 package project
 
 import (
+	"errors"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 // runInShell runs the specified command using the system shell. On Windows
-// systems, this is %COMSPEC% (with a fallback to cmd.exe if unspecified).
+// systems, this is %ComSpec% (with a fallback to a fully qualified cmd.exe if
+// %ComSpec% is not an absolute path (which includes cases where it's empty)).
 func runInShell(command string) error {
 	// Determine the shell to use.
-	shell := os.Getenv("COMSPEC")
-	if shell == "" {
-		shell = "cmd.exe"
+	shell := os.Getenv("ComSpec")
+	if !filepath.IsAbs(shell) {
+		systemRoot := os.Getenv("SystemRoot")
+		if !filepath.IsAbs(systemRoot) {
+			return errors.New("invalid ComSpec and SystemRoot environment variables")
+		}
+		shell = filepath.Join(systemRoot, "System32", "cmd.exe")
 	}
 
 	// Set up the process.
